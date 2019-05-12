48cccbe74936a869e764b81242942633a3a03221
tylim <limteikyee@gmail.com>
2019-05-13 02:20:03 +0800

    custom service worker setup phase one

--- src/index.js
+++ src/index.js
@@ -1,34 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
-import "assets/css/nucleo-icons.css";
-import "assets/scss/blk-design-system-react.scss?v=1.0.0";
-import "assets/demo/demo.css";
+import 'assets/css/nucleo-icons.css'
+import 'assets/scss/blk-design-system-react.scss?v=1.0.0'
+import 'assets/demo/demo.css'
 
-import Index from "views/Index.jsx";
-import LandingPage from "views/examples/LandingPage.jsx";
-import RegisterPage from "views/examples/RegisterPage.jsx";
-import ProfilePage from "views/examples/ProfilePage.jsx";
+import Index from 'views/Index.jsx'
+import LandingPage from 'views/examples/LandingPage.jsx'
+import RegisterPage from 'views/examples/RegisterPage.jsx'
+import ProfilePage from 'views/examples/ProfilePage.jsx'
+
+import * as serviceWorker from 'serviceWorker'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/components" render={props => <Index {...props} />} />
-      <Route
-        path="/landing-page"
-        render={props => <LandingPage {...props} />}
-      />
-      <Route
-        path="/register-page"
-        render={props => <RegisterPage {...props} />}
-      />
-      <Route
-        path="/profile-page"
-        render={props => <ProfilePage {...props} />}
-      />
-      <Redirect from="/" to="/components" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+	<BrowserRouter>
+		<Switch>
+			<Route path='/components' render={props => <Index {...props} />} />
+			<Route
+				path='/landing-page'
+				render={props => <LandingPage {...props} />}
+			/>
+			<Route
+				path='/register-page'
+				render={props => <RegisterPage {...props} />}
+			/>
+			<Route
+				path='/profile-page'
+				render={props => <ProfilePage {...props} />}
+			/>
+			<Redirect from='/' to='/components' />
+		</Switch>
+	</BrowserRouter>,
+	document.getElementById('root')
+)
+
+serviceWorker.register()
