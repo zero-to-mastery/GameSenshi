36ac150fae40ef4474886013014a8a7e58e2957d
tylim <limteikyee@gmail.com>
2019-05-13 17:47:27 +0800

    hide unused section

--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -6,17 +6,17 @@ import PageHeader from 'components/PageHeader/PageHeader.jsx'
 import Footer from 'components/Footer/Footer.jsx'
 
 // sections for this page/view
-import Basics from 'views/IndexSections/Basics.jsx'
-import Navbars from 'views/IndexSections/Navbars.jsx'
-import Tabs from 'views/IndexSections/Tabs.jsx'
-import Pagination from 'views/IndexSections/Pagination.jsx'
-import Notifications from 'views/IndexSections/Notifications.jsx'
-import Typography from 'views/IndexSections/Typography.jsx'
-import JavaScript from 'views/IndexSections/JavaScript.jsx'
-import NucleoIcons from 'views/IndexSections/NucleoIcons.jsx'
-import Signup from 'views/IndexSections/Signup.jsx'
-import Examples from 'views/IndexSections/Examples.jsx'
-import Download from 'views/IndexSections/Download.jsx'
+// import Basics from 'views/IndexSections/Basics.jsx'
+// import Navbars from 'views/IndexSections/Navbars.jsx'
+// import Tabs from 'views/IndexSections/Tabs.jsx'
+// import Pagination from 'views/IndexSections/Pagination.jsx'
+// import Notifications from 'views/IndexSections/Notifications.jsx'
+// import Typography from 'views/IndexSections/Typography.jsx'
+// import JavaScript from 'views/IndexSections/JavaScript.jsx'
+// import NucleoIcons from 'views/IndexSections/NucleoIcons.jsx'
+// import Signup from 'views/IndexSections/Signup.jsx'
+// import Examples from 'views/IndexSections/Examples.jsx'
+// import Download from 'views/IndexSections/Download.jsx'
 
 class Index extends React.Component {
 	componentDidMount() {
@@ -31,7 +31,7 @@ class Index extends React.Component {
 				<IndexNavbar />
 				<div className='wrapper'>
 					<PageHeader />
-					<div className='main'>
+					{/* <div className='main'>
 						<Basics />
 						<Navbars />
 						<Tabs />
@@ -43,7 +43,7 @@ class Index extends React.Component {
 						<Signup />
 						<Examples />
 						<Download />
-					</div>
+					</div> */}
 					<Footer />
 				</div>
 			</>
