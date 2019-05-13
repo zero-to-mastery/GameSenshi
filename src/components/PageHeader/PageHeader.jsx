50cf04ef4cb2d9feb80f665407fdd47576bdcc13
tylim <limteikyee@gmail.com>
2019-05-13 16:15:41 +0800

    linting

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,31 +1,31 @@
-import React from "react";
+import React from 'react'
 
 // reactstrap components
-import { Container } from "reactstrap";
+import { Container } from 'reactstrap'
 
 class PageHeader extends React.Component {
-  render() {
-    return (
-      <div className="page-header header-filter">
-        <div className="squares square1" />
-        <div className="squares square2" />
-        <div className="squares square3" />
-        <div className="squares square4" />
-        <div className="squares square5" />
-        <div className="squares square6" />
-        <div className="squares square7" />
-        <Container>
-          <div className="content-center brand">
-            <h1 className="h1-seo">BLK• React</h1>
-            <h3 className="d-none d-sm-block">
-              A beautiful Design System for Bootstrap 4 (reactstrap) and React.
-              It's Free and Open Source.
-            </h3>
-          </div>
-        </Container>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='page-header header-filter'>
+				<div className='squares square1' />
+				<div className='squares square2' />
+				<div className='squares square3' />
+				<div className='squares square4' />
+				<div className='squares square5' />
+				<div className='squares square6' />
+				<div className='squares square7' />
+				<Container>
+					<div className='content-center brand'>
+						<h1 className='h1-seo'>BLK• React</h1>
+						<h3 className='d-none d-sm-block'>
+							A beautiful Design System for Bootstrap 4 (reactstrap) and React.
+							It's Free and Open Source.
+						</h3>
+					</div>
+				</Container>
+			</div>
+		)
+	}
 }
 
-export default PageHeader;
+export default PageHeader
