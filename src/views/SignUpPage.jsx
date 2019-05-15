458cfb22e3dc0b8b9dc055feab2408962dce8894
tylim <limteikyee@gmail.com>
2019-05-16 05:01:31 +0800

    implementing formik phase 1

--- src/views/SignUpPage.jsx
+++ src/views/SignUpPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import classnames from 'classnames'
 import { Link } from 'react-router-dom'
+import firebase from 'firebase'
+import { Formik, yupToFormErrors } from 'formik'
+import { string, email, object } from 'yup'
 
 // reactstrap components
 import {
@@ -63,7 +66,7 @@ class SignUpPage extends React.Component {
 			<>
 				<IndexNavbar />
 				<div className='wrapper'>
-					<div className='page-header' style={{ 'max-height': '1300px' }}>
+					<div className='page-header'>
 						<div className='page-header-image' />
 						<div className='content'>
 							<Container>
@@ -93,148 +96,202 @@ class SignUpPage extends React.Component {
 													<h3 className='mb-0'>Sign up with</h3>
 												</div>
 											</CardBody>
-											<CardBody>
-												<div className='btn-wrapper text-center'>
-													<Button
-														size='lg'
-														className='btn-icon btn-round'
-														color='facebook'
-														href=''
-														id='facebook'
-														target='_blank'>
-														<i className='fab fa-facebook-square' />
-													</Button>
-													<UncontrolledTooltip delay={0} target='facebook'>
-														Sign up with Facebook!
-													</UncontrolledTooltip>
-													<Button
-														size='lg'
-														className='btn-icon btn-round'
-														color='warning'
-														href=''
-														id='google'
-														target='_blank'>
-														<i className='fab fa-google' />
-													</Button>
-													<UncontrolledTooltip delay={0} target='google'>
-														Sign up with Google!
-													</UncontrolledTooltip>
-													<Button
-														size='lg'
-														className='btn-icon btn-round'
-														color='twitter'
-														href=''
-														id='twitter'
-														target='_blank'>
-														<i className='fab fa-twitter' />
-													</Button>
-													<UncontrolledTooltip delay={0} target='twitter'>
-														Sign up with Twitter!
-													</UncontrolledTooltip>
-												</div>
-												<Row>
-													<Col />
-													<Col className='text-center text-muted mb-4 mt-3 col-auto'>
-														<small>Or Classically</small>
-													</Col>
-													<Col />
-												</Row>
-												<Form className='form'>
-													<InputGroup
-														className={classnames({
-															'input-group-focus': this.state.fullNameFocus,
-														})}>
-														<InputGroupAddon addonType='prepend'>
-															<InputGroupText>
-																<i className='tim-icons icon-single-02' />
-															</InputGroupText>
-														</InputGroupAddon>
-														<Input
-															placeholder='Username'
-															type='text'
-															onFocus={e =>
-																this.setState({ fullNameFocus: true })
+											<Formik
+												initialValues={{ email: '', password: '' }}
+												onSubmit={(values, actions) => {
+													firebase
+														.auth()
+														.createUserWithEmailAndPassword(
+															values.email,
+															values.password
+														)
+														.then(credential => {
+															actions.setSubmitting(false)
+															if (
+																credential.user &&
+																credential.user.emailVerified === false
+															) {
+																credential.user
+																	.sendEmailVerification()
+																	.then(function() {
+																		console.log(
+																			'email verification sent to user'
+																		)
+																	})
 															}
-															onBlur={e =>
-																this.setState({ fullNameFocus: false })
-															}
-														/>
-													</InputGroup>
-													<InputGroup
-														className={classnames({
-															'input-group-focus': this.state.emailFocus,
-														})}>
-														<InputGroupAddon addonType='prepend'>
-															<InputGroupText>
-																<i className='tim-icons icon-email-85' />
-															</InputGroupText>
-														</InputGroupAddon>
-														<Input
-															placeholder='Email'
-															type='text'
-															onFocus={e => this.setState({ emailFocus: true })}
-															onBlur={e => this.setState({ emailFocus: false })}
-														/>
-													</InputGroup>
-													<InputGroup
-														className={classnames({
-															'input-group-focus': this.state.passwordFocus,
-														})}>
-														<InputGroupAddon addonType='prepend'>
-															<InputGroupText>
-																<i className='tim-icons icon-lock-circle' />
-															</InputGroupText>
-														</InputGroupAddon>
-														<Input
-															placeholder='Password'
-															type='text'
-															onFocus={e =>
-																this.setState({ passwordFocus: true })
-															}
-															onBlur={e =>
-																this.setState({ passwordFocus: false })
-															}
-														/>
-													</InputGroup>
-													<FormGroup check className='text-left'>
-														<Label check>
-															<Input type='checkbox' />
-															<span className='form-check-sign' />I agree to the{' '}
-															<a
-																href='#pablo'
-																onClick={e => e.preventDefault()}>
-																terms and conditions
-															</a>
-															.
-														</Label>
-													</FormGroup>
-												</Form>
-											</CardBody>
-											<CardFooter>
-												<Row>
-													<Col />
-													<Col>
-														<Button
-															className='btn-round'
-															color='primary'
-															size='lg'>
-															Get Started
-														</Button>
-													</Col>
-													<Col />
-												</Row>
-												<Row>
-													<Col />
-													<Col className='col-auto'>
-														<Label check>
-															<span className='form-check-sign' />
-															Owned an account?{' '}
-															<Link to='/signIn'>Sign in</Link> instead.
-														</Label>
-													</Col>
-													<Col />
-												</Row>
-											</CardFooter>
+														})
+														.catch(error => {
+															console.log(error)
+															actions.setSubmitting(false)
+															actions.setErrors({ email: '', password: '' })
+														})
+												}}
+												validate={values => {
+													const schema = object().shape({
+														email: string()
+															.email('Invalid email')
+															.required('No email is provided'),
+														password: string()
+															.required('No password provided.')
+															.min(
+																8,
+																'Password is too short - should be 8 chars minimum.'
+															),
+													})
+													return schema.validate(values, { abortEarly: false }) // return promise
+												}}>
+												{({
+													values,
+													errors,
+													dirty,
+													touched,
+													handleChange,
+													handleBlur,
+													handleSubmit,
+													isSubmitting,
+												}) => (
+													<>
+														<CardBody>
+															<div className='btn-wrapper text-center'>
+																<Button
+																	size='lg'
+																	className='btn-icon btn-round'
+																	color='facebook'
+																	href=''
+																	id='facebook'
+																	target='_blank'>
+																	<i className='fab fa-facebook-square' />
+																</Button>
+																<UncontrolledTooltip
+																	delay={0}
+																	target='facebook'>
+																	Sign up with Facebook!
+																</UncontrolledTooltip>
+																<Button
+																	size='lg'
+																	className='btn-icon btn-round'
+																	color='warning'
+																	href=''
+																	id='google'
+																	target='_blank'>
+																	<i className='fab fa-google' />
+																</Button>
+																<UncontrolledTooltip delay={0} target='google'>
+																	Sign up with Google!
+																</UncontrolledTooltip>
+																<Button
+																	size='lg'
+																	className='btn-icon btn-round'
+																	color='twitter'
+																	href=''
+																	id='twitter'
+																	target='_blank'>
+																	<i className='fab fa-twitter' />
+																</Button>
+																<UncontrolledTooltip delay={0} target='twitter'>
+																	Sign up with Twitter!
+																</UncontrolledTooltip>
+															</div>
+															<Row>
+																<Col />
+																<Col className='text-center text-muted mb-4 mt-3 col-auto'>
+																	<small>Or Classically</small>
+																</Col>
+																<Col />
+															</Row>
+															<Form className='form'>
+																<InputGroup
+																	className={classnames({
+																		'input-group-focus': this.state.emailFocus,
+																	})}>
+																	<InputGroupAddon addonType='prepend'>
+																		<InputGroupText>
+																			<i className='tim-icons icon-email-85' />
+																		</InputGroupText>
+																	</InputGroupAddon>
+																	<Input
+																		placeholder='Email Address'
+																		type='text'
+																		name='email'
+																		onChange={handleChange}
+																		onFocus={e =>
+																			this.setState({ emailFocus: true })
+																		}
+																		onBlur={e => {
+																			handleBlur(e)
+																			this.setState({ emailFocus: false })
+																		}}
+																		value={values.email}
+																	/>
+																</InputGroup>
+																<InputGroup
+																	className={classnames({
+																		'input-group-focus': this.state
+																			.passwordFocus,
+																	})}>
+																	<InputGroupAddon addonType='prepend'>
+																		<InputGroupText>
+																			<i className='tim-icons icon-lock-circle' />
+																		</InputGroupText>
+																	</InputGroupAddon>
+																	<Input
+																		placeholder='Password'
+																		type='text'
+																		name='password'
+																		onFocus={e =>
+																			this.setState({ passwordFocus: true })
+																		}
+																		onBlur={e => {
+																			handleBlur(e)
+																			this.setState({ passwordFocus: false })
+																		}}
+																		value={values.password}
+																	/>
+																</InputGroup>
+																<FormGroup check className='text-left'>
+																	<Label check>
+																		<Input type='checkbox' />
+																		<span className='form-check-sign' />I agree
+																		to the{' '}
+																		<a
+																			href='#pablo'
+																			onClick={e => e.preventDefault()}>
+																			terms and conditions
+																		</a>
+																	</Label>
+																</FormGroup>
+															</Form>
+														</CardBody>
+														<CardFooter>
+															<Row>
+																<Col />
+																<Col>
+																	<Button
+																		className='btn-round'
+																		color='primary'
+																		size='lg'
+																		disabled={isSubmitting}
+																		onClick={handleSubmit}>
+																		Sign Up
+																	</Button>
+																</Col>
+																<Col />
+															</Row>
+															<Row>
+																<Col />
+																<Col className='col-auto'>
+																	<Label check>
+																		<span className='form-check-sign' />
+																		Already a member?{' '}
+																		<Link to='/signIn'>Sign in</Link>
+																	</Label>
+																</Col>
+																<Col />
+															</Row>
+														</CardFooter>{' '}
+													</>
+												)}
+											</Formik>
 										</Card>
 									</Col>
 								</Row>
