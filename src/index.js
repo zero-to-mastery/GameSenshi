74b9298991b905cb614228cc745a64b2258c7d1b
tylim <limteikyee@gmail.com>
2019-05-16 05:00:45 +0800

    initialize firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import firebase from 'firebase/app'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import * as serviceWorker from 'serviceWorker'
 
 import 'assets/css/nucleo-icons.css'
 import 'assets/scss/blk-design-system-react.scss?v=1.0.0'
@@ -12,7 +14,17 @@ import SignUpPage from 'views/SignUpPage'
 import ProfilePage from 'views/ProfilePage'
 import SignInPage from 'views/SignInPage'
 
-import * as serviceWorker from 'serviceWorker'
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+}
+
+firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(
 	<BrowserRouter>
