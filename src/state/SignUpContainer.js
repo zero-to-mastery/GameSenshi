c9a9b07e787cfc3b02ba0ebdffe145fc572b0a22
tylim <limteikyee@gmail.com>
2019-05-21 01:17:09 +0800

    replaced by local state

--- src/state/SignUpContainer.js
+++ src/state/SignUpContainer.js
@@ -1,11 +1,7 @@
 import { Container } from 'unstated'
 
 class SignUpContainer extends Container {
-	state = {
-		validating: true,
-		delay: 0,
-		timeOutID: 0,
-	}
+	state = {}
 }
 
 export default SignUpContainer
