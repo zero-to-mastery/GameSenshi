dd48ae75efa8313d0faaf94170265b018dceeaf3
tylim <limteikyee@gmail.com>
2019-05-22 19:54:36 +0800

    modularize validation so that it can share to backend

---
+++ src/utils/validation.js
@@ -0,0 +1,34 @@
+// in future this should turn into npm library so that backend can use the same code using absolute path
+// need proper dependency management system
+
+import { string, boolean } from 'yup'
+import {
+	EMAIL_VALIDATION,
+	PASSWORD_VALIDATION,
+	TERM_VALIDATION,
+} from 'utils/signUpConstants'
+
+const signUpValidation = {
+	[EMAIL_VALIDATION]: value =>
+		string()
+			.required('Email is required')
+			.email('Email not valid')
+			.validate(value, {
+				abortEarly: false,
+			}),
+	[PASSWORD_VALIDATION]: value =>
+		string()
+			.required('Password is required')
+			.min(8, 'Password must be 8 characters or longer')
+			.validate(value, {
+				abortEarly: false,
+			}),
+	[TERM_VALIDATION]: value =>
+		boolean()
+			.oneOf([true], 'Must Accept Terms and Conditions')
+			.validate(value, {
+				abortEarly: false,
+			}),
+}
+
+export { signUpValidation }
