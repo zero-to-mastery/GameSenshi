467983d18a43607c7dfb8ccdc3075fd7f8c75c5e
tylim <limteikyee@gmail.com>
2019-05-23 19:00:15 +0800

    now can flexibly set envrionment variable for firebase

---
+++ functions/src/utils/envKeyConstants.js
@@ -0,0 +1,20 @@
+const ENV = 'env'
+const REACT_APP_ = 'react_app_'
+const REACT_APP_API_KEY = REACT_APP_ + 'api_key'
+const REACT_APP_AUTH_DOMAIN = REACT_APP_ + 'auth_domain'
+const REACT_APP_DATABASE_URL = REACT_APP_ + 'database_url'
+const REACT_APP_PROJECT_ID = REACT_APP_ + 'project_id'
+const REACT_APP_STORAGE_BUCKET = REACT_APP_ + 'storage_bucket'
+const REACT_APP_MESSAGING_SENDER_ID = REACT_APP_ + 'messaging_sender_id'
+const REACT_APP_APP_ID = REACT_APP_ + 'app_id'
+
+export {
+	ENV,
+	REACT_APP_API_KEY,
+	REACT_APP_AUTH_DOMAIN,
+	REACT_APP_DATABASE_URL,
+	REACT_APP_PROJECT_ID,
+	REACT_APP_STORAGE_BUCKET,
+	REACT_APP_MESSAGING_SENDER_ID,
+	REACT_APP_APP_ID,
+}
---
+++ functions/uploadEnv.js
@@ -0,0 +1,77 @@
+import child_process from 'child_process'
+
+import {
+	ENV,
+	REACT_APP_API_KEY,
+	REACT_APP_AUTH_DOMAIN,
+	REACT_APP_DATABASE_URL,
+	REACT_APP_PROJECT_ID,
+	REACT_APP_STORAGE_BUCKET,
+	REACT_APP_MESSAGING_SENDER_ID,
+	REACT_APP_APP_ID,
+} from './src/utils/envKeyConstants'
+
+const spawn = child_process.spawn
+
+const {
+	[REACT_APP_API_KEY]: react_app_api_key,
+	[REACT_APP_AUTH_DOMAIN]: react_app_auth_domain,
+	[REACT_APP_DATABASE_URL]: react_app_database_url,
+	[REACT_APP_PROJECT_ID]: react_app_project_id,
+	[REACT_APP_STORAGE_BUCKET]: react_app_storage_bucket,
+	[REACT_APP_MESSAGING_SENDER_ID]: react_app_messaging_sender_id,
+	[REACT_APP_APP_ID]: react_app_app_id,
+} = process.env
+
+const obj = {
+	[ENV]: {
+		[REACT_APP_API_KEY]: react_app_api_key,
+		[REACT_APP_AUTH_DOMAIN]: react_app_auth_domain,
+		[REACT_APP_DATABASE_URL]: react_app_database_url,
+		[REACT_APP_PROJECT_ID]: react_app_project_id,
+		[REACT_APP_STORAGE_BUCKET]: react_app_storage_bucket,
+		[REACT_APP_MESSAGING_SENDER_ID]: react_app_messaging_sender_id,
+		[REACT_APP_APP_ID]: react_app_app_id,
+	},
+}
+const isObj = x => {
+	return x !== null && typeof x === 'object'
+}
+
+const parse = tree => {
+	const values = []
+	const properties = Object.keys(tree)
+	properties.forEach(prop => {
+		if (isObj(tree[prop])) {
+			const childrens = parse(tree[prop])
+			childrens.forEach(child => {
+				const value = prop + '.' + child
+				values.push(value)
+			})
+		} else {
+			const value = prop + '=' + '"' + tree[prop] + '"'
+			values.push(value)
+		}
+	})
+	return values
+}
+
+const runFirebaseConfigSet = properties => {
+	return new Promise((resolve, reject) => {
+		const args = ['functions:config:set'].concat(properties)
+		const cmd = spawn('firebase', args, { shell: true })
+		cmd.stdout.setEncoding('utf8')
+		cmd.stdout.on('data', data => {
+			console.log(data)
+		})
+		cmd.stderr.on('data', data => {
+			console.log('Error:', cmd.stderr.toString())
+		})
+		cmd.on('close', code => {
+			console.log(`Exit code: ${code}`)
+			resolve(code)
+		})
+	})
+}
+console.log(parse(obj))
+runFirebaseConfigSet(parse(obj))
