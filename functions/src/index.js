9d93205904183d3d88be2400c5d04e918f77d068
tylim <limteikyee@gmail.com>
2019-05-24 14:19:15 +0800

    refactor

--- functions/src/index.js
+++ functions/src/index.js
@@ -2,30 +2,11 @@
 // https://firebase.google.com/docs/functions/write-firebase-functions
 import * as functions from 'firebase-functions' // https://stackoverflow.com/questions/51118943/cannot-read-property-https-of-undefined-error-in-firebase-functions
 import admin from 'firebase-admin'
-import { firebase } from 'utils/firebase'
-import { EMAIL, PASSWORD } from 'utils/signUpConstants'
+import '@babel/polyfill' // https://stackoverflow.com/questions/49253746/error-regeneratorruntime-is-not-defined-with-babel-7
+import { handleSignUp, handleVerifyEmail } from 'api'
 
 admin.initializeApp()
 
-exports.signUp = functions.https.onCall((data, context) => {
-	const { [EMAIL]: email, [PASSWORD]: password } = data
-	firebase
-		.auth()
-		.createUserWithEmailAndPassword(email, password)
-		.then(credential => {
-			if (credential.user && credential.user.emailVerified === false) {
-				credential.user
-					.sendEmailVerification()
-					.then(() => {
-						console.log('email verification sent to user')
-					})
-					.catch(error => {
-						console.log('email user failed', error)
-					})
-			}
-		})
-		.catch(error => {
-			console.log('submit failed', error)
-		})
-	return { data: EMAIL }
-})
+const signUp = functions.https.onCall(handleSignUp)
+
+export { signUp }
