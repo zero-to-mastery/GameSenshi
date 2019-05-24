0c79a03b020efc77d8b9cee0cdd74abb232f2ac4
tylim <limteikyee@gmail.com>
2019-05-24 22:42:44 +0800

    email existence verification now work fine

--- functions/src/api/handleVerifyEmail.js
+++ functions/src/api/handleVerifyEmail.js
@@ -1,21 +1,29 @@
 import * as functions from 'firebase-functions'
-import got from 'got'
+import req from 'request-promise'
 import { resObj } from 'utils/objects'
 import { ENV, VERIFY_EMAIL_API_KEY } from 'utils/envKeyConstants'
+import { EMAIL } from 'utils/signUpConstants'
 
 const {
 	[ENV]: { [VERIFY_EMAIL_API_KEY]: verify_email_api_key },
 } = functions.config()
 
-const handleVerifyEmail = email =>
-	got(`${verify_email_api_key}${email}`)
+// this function should merge with on sign up
+// it is waste to check this when user finish typing then check again when user submit
+// a better way is to replace this with check existing email in database
+const handleVerifyEmail = data => {
+	return req(`${verify_email_api_key}${data[EMAIL]}`)
 		.then(res => {
-			if (res.body.status === 1) {
+			const data = JSON.parse(res)
+			if (data.status === 1) {
 				return resObj(true)
 			} else {
 				return resObj(false, ['Invalid Email'])
 			}
 		})
-		.catch(err => resObj(false, [err.response.body]))
+		.catch(err => {
+			return resObj(false, ['Internal Error Code 1'], 1)
+		})
+}
 
 export default handleVerifyEmail
