6db0064bcb1b8ec7df864713e1c81b34057bdee5
tylim <limteikyee@gmail.com>
2019-05-25 03:47:40 +0800

    add validations, 1. input validation 2. check is user exist (new) 3. check is email is real (new)

--- functions/src/api/handleSignUp.js
+++ functions/src/api/handleSignUp.js
@@ -1,7 +1,12 @@
+import * as functions from 'firebase-functions'
+import req from 'request-promise'
+
 import { firebase } from 'utils/firebase'
 import { signUpValidation } from 'utils/validation'
 import { resObj, valObj } from 'utils/objects'
+import { handleIsUserExist } from 'api'
 
+import { ENV, VERIFY_EMAIL_API_KEY } from 'utils/envKeyConstants'
 import {
 	EMAIL,
 	PASSWORD,
@@ -10,6 +15,10 @@ import {
 	PASSWORD_VALIDATION,
 	TERM_VALIDATION,
 } from 'utils/signUpConstants'
+
+const {
+	[ENV]: { [VERIFY_EMAIL_API_KEY]: verify_email_api_key },
+} = functions.config()
 
 const handleSignUp = async (data, context) => {
 	const {
@@ -20,18 +29,47 @@ const handleSignUp = async (data, context) => {
 
 	const { [EMAIL]: email, [PASSWORD]: password, [TERM]: term } = data
 
-	const emailInvalid = await emailValidation(email)
-		.then(() => '')
-		.catch(result => valObj(EMAIL, result.errors))
-	const passwordInvalid = await passwordValidation(password)
-		.then(() => '')
-		.catch(result => valObj(PASSWORD, result.errors))
-	const termInvalid = await termValidation(term)
-		.then(() => '')
-		.catch(result => valObj(TERM, result.errors))
-	if (emailInvalid || passwordInvalid || termInvalid) {
-		return resObj(false, [emailInvalid, passwordInvalid, termInvalid])
-	} else {
+	try {
+		const emailInvalid = await emailValidation(email)
+			.then(() => '')
+			.catch(result => valObj(EMAIL, result.errors))
+		const passwordInvalid = await passwordValidation(password)
+			.then(() => '')
+			.catch(result => valObj(PASSWORD, result.errors))
+		const termInvalid = await termValidation(term)
+			.then(() => '')
+			.catch(result => valObj(TERM, result.errors))
+
+		if (emailInvalid || passwordInvalid || termInvalid) {
+			return resObj(
+				false,
+				emailInvalid.message || passwordInvalid.message || termInvalid.message
+			)
+		}
+		const isUserExist = await handleIsUserExist(data)
+
+		if (!isUserExist.status) {
+			return isUserExist
+		}
+
+		const isEmailExist = await req(`${verify_email_api_key}${email}`)
+			.then(res => {
+				const data = JSON.parse(res)
+				if (data.status === 1) {
+					return resObj(true)
+				} else {
+					return resObj(false, 'Invalid Email')
+				}
+			})
+			.catch(errors => {
+				console.log('email verifying error', errors)
+				return resObj(false, 'Internal Error Code 2', 2)
+			})
+
+		if (!isEmailExist.status) {
+			return isEmailExist
+		}
+
 		return firebase
 			.auth()
 			.createUserWithEmailAndPassword(email, password)
@@ -49,9 +87,12 @@ const handleSignUp = async (data, context) => {
 				return resObj(true)
 			})
 			.catch(errors => {
-				console.log('submit failed', errors)
-				return resObj(false, [errors])
+				console.log('submit error', errors)
+				return resObj(false, 'Internal Error Code 3', 3)
 			})
+	} catch (e) {
+		console.log(e)
+		return resObj(false, 'Internal Error Code 4', 4)
 	}
 }
 
