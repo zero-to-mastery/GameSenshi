aa28f2f83c79e8ac98c906be147687f7de1f8508
tylim <limteikyee@gmail.com>
2019-05-27 03:13:24 +0800

    return the same shape object as initial value

--- src/api/handleSignup.js
+++ src/api/handleSignup.js
@@ -5,14 +5,15 @@ import { STATUS, DATA } from 'utils/commonConstants'
 
 const handleSignUp = values => {
 	const { [EMAIL]: email, [PASSWORD]: password, [TERM]: term } = values
-
+	const defaultValues = { [EMAIL]: '', [PASSWORD]: '', [TERM]: '' }
 	const signUpUser = functions.httpsCallable(ON_SIGN_UP)
 	signUpUser({ email, password, term }).then(res => {
 		console.log(res)
 		// return undefined if success
 		// return object with property same with initial values
 		if (!res[DATA][STATUS]) {
-			return res[DATA][DATA]
+			console.log({ ...defaultValues, ...res[DATA][DATA] })
+			return { ...defaultValues, ...res[DATA][DATA] }
 		} else {
 			// if success, return to history page if available
 			// if not, return to index page
