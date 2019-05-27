4db7efb49c7aa3db0379cd56bb4500274c335394
tylim <limteikyee@gmail.com>
2019-05-28 02:02:26 +0800

    simplifying

--- src/api/handleSignup.js
+++ src/api/handleSignup.js
@@ -1,21 +1,20 @@
 import { functions } from 'utils/firebase'
 
 import { EMAIL, PASSWORD, TERM, ON_SIGN_UP } from 'utils/signUpConstants'
-import { DATA, STATUS } from 'utils/commonConstants'
+import { DATA } from 'utils/commonConstants'
 
 const handleSignUp = values => {
 	const { [EMAIL]: email, [PASSWORD]: password, [TERM]: term } = values
 	const defaultValues = {
+		// undefined = success
 		[EMAIL]: undefined,
 		[PASSWORD]: undefined,
 		[TERM]: undefined,
 	}
 	const signUpUser = functions.httpsCallable(ON_SIGN_UP)
 	return signUpUser({ email, password, term }).then(res => {
-		return {
-			[STATUS]: res[DATA][STATUS],
-			[DATA]: { ...defaultValues, ...res[DATA][DATA] },
-		}
+		res[DATA][DATA] = { ...defaultValues, ...res[DATA][DATA] }
+		return res[DATA]
 	})
 }
 
