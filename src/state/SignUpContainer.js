af16d9ec90248c595f72e3f257989977b297626b
tylim <limteikyee@gmail.com>
2019-05-27 17:30:38 +0800

    resue the constants because issue invalid meta tag persist after submit and we need to change it from outside

--- src/state/SignUpContainer.js
+++ src/state/SignUpContainer.js
@@ -1,12 +1,28 @@
 import { Container } from 'unstated'
-import { SUBMITTING } from 'utils/signUpConstants'
+import {
+	EMAIL,
+	SUBMITTING,
+	EMAIL_VALID,
+	PASSWORD_VALID,
+	TERM_VALID,
+	EMAIL_SUBMIT_ERRORS,
+	PASSWORD_SUBMIT_ERRORS,
+	TERM_SUBMIT_ERRORS,
+} from 'utils/signUpConstants'
 
 import { WILL_UNMOUNT } from 'utils/commonConstants'
 
 class SignUpContainer extends Container {
 	state = {
+		[EMAIL]: '',
+		[EMAIL_VALID]: false,
+		[PASSWORD_VALID]: false,
+		[TERM_VALID]: false,
 		[SUBMITTING]: false,
 		[WILL_UNMOUNT]: false,
+		[EMAIL_SUBMIT_ERRORS]: undefined,
+		[PASSWORD_SUBMIT_ERRORS]: undefined,
+		[TERM_SUBMIT_ERRORS]: undefined,
 	}
 }
 
