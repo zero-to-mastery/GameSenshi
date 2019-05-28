acf0c599187aa81926d7bd4b049bddefb2d8842e
tylim <limteikyee@gmail.com>
2019-05-28 21:58:49 +0800

    remove images from initial caching

--- src/sw-build.js
+++ src/sw-build.js
@@ -7,7 +7,7 @@ const buildSW = () => {
 			swSrc: 'src/sw-template.js', // this is your sw template file
 			swDest: 'build/sw.js', // this will be created in the build step
 			globDirectory: 'build',
-			globPatterns: ['**/*.{js,css,html,png}'],
+			globPatterns: ['**/*.{js,css,html}'],
 		})
 		.then(({ count, size, warnings }) => {
 			// Optionally, log any warnings and details.
