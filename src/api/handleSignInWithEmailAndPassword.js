ac76877add0d0605c81d5402ede298eb4c6353a3
tylim <limteikyee@gmail.com>
2019-05-28 22:33:15 +0800

    remove extra code

--- src/api/handleSignInWithEmailAndPassword.js
+++ src/api/handleSignInWithEmailAndPassword.js
@@ -1,29 +1,17 @@
 import { auth } from 'utils/firebase'
 
 const handleSignInWithEmailAndPassword = async (email, password) => {
-	// even without explicitly set persistence, the persistence is still LOCAL
-	// because sign in set local persistence by default
-	// change LOCAL to SESSION or NONE if you want different persistence
+	// sign in set local persistence by default, allowing user to auto sign in
 	// https://firebase.google.com/docs/auth/web/auth-state-persistence?authuser=0
-	const setPersistence = auth()
-		.setPersistence(auth.Auth.Persistence.LOCAL)
+
+	return auth()
+		.signInWithEmailAndPassword(email, password)
 		.catch(err => {
 			// Handle Errors here.
-			console.log('set persistence failed', err)
+			console.log('sign in failed', err)
 			return false
+			// ...
 		})
-
-	return (
-		(await setPersistence) !== false &&
-		auth()
-			.signInWithEmailAndPassword(email, password)
-			.catch(err => {
-				// Handle Errors here.
-				console.log('sign in failed', err)
-				return false
-				// ...
-			})
-	)
 }
 
 export default handleSignInWithEmailAndPassword
