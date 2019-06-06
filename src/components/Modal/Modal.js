2fbcd4167282668ba5cf175621eaee30e71d55db
tylim <limteikyee@gmail.com>
2019-06-06 12:26:24 +0800

    basic setup of modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,12 @@
+// the purpose of this modal is to link user social auth
+import React from 'react'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+
+// state
+import { alertStore, Subscribe } from 'state'
+
+const SocialAuthSignUpModal = () => {
+	return alertStore
+}
+
+export default SocialAuthSignUpModal
