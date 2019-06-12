8e9dd85a9f00c12b73c4e59afb93bfd8b1e1ba6b
tylim <limteikyee@gmail.com>
2019-06-12 12:29:12 +0800

    housekeeping

--- src/state/index.js
+++ src/state/index.js
@@ -3,11 +3,18 @@ import UserContainer from 'state/UserContainer'
 import AlertContainer from 'state/AlertContainer'
 import SocialAuthModalContainer from 'state/SocialAuthModalContainer'
 
-import { Subscribe } from 'unstated'
+import { Subscribe, Provider } from 'unstated'
 
 const authStore = new AuthContainer()
 const userStore = new UserContainer()
 const alertStore = new AlertContainer()
 const socialAuthModalStore = new SocialAuthModalContainer()
 
-export { authStore, alertStore, userStore, socialAuthModalStore, Subscribe }
+export {
+	authStore,
+	alertStore,
+	userStore,
+	socialAuthModalStore,
+	Subscribe,
+	Provider,
+}
