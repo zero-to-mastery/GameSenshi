d46e1ddb60a5d9781e0a6d1cdc02d9200fed1c08
tylim <limteikyee@gmail.com>
2019-06-13 21:51:16 +0800

    implment sign in validation and functionality

--- src/views/SignInPage.jsx
+++ src/views/SignInPage.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
-// nodejs library that concatenates classes
-import classnames from 'classnames'
+
+// routing
+import { withLastLocation } from 'react-router-last-location'
+import { onSignedInRouting } from 'routes'
+
+// api
+import { handleSignInWithEmailAndPassword } from 'api'
+
+// validation
+import {
+	signInEmailValidation,
+	signInPasswordValidation,
+} from 'utils/validation'
+
 // reactstrap components
 import {
 	Button,
@@ -11,17 +23,39 @@ import {
 	CardImg,
 	CardTitle,
 	Form,
-	Input,
-	InputGroupAddon,
-	InputGroupText,
-	InputGroup,
 	Container,
 	Col,
 	Row,
 } from 'reactstrap'
+import Loader from 'react-loader-spinner'
+
+// form validation
+import { Form as FinalForm } from 'react-final-form'
+import { FORM_ERROR } from 'final-form'
+import createDecorator from 'final-form-focus'
+
+// constants
+import {
+	EMAIL,
+	USERNAME,
+	PASSWORD,
+	USER_EMAIL,
+	USER_SIGNED_IN,
+	USER_DISPLAY_NAME,
+} from 'constantValues'
 
 // core components
-import { Footer, IndexNavbar, SocialAuthButtonGroup } from 'components'
+import {
+	Footer,
+	IndexNavbar,
+	SocialAuthButtonGroup,
+	InputField,
+} from 'components'
+
+// state management
+import { authStore, userStore } from 'state'
+
+const focusOnError = createDecorator()
 
 class LoginPage extends React.Component {
 	state = {}
@@ -33,9 +67,29 @@ class LoginPage extends React.Component {
 	componentWillUnmount() {
 		document.body.classList.remove('login-page')
 	}
+
+	onSubmit = async (values, history, lastLocation) => {
+		const signInFailed = await handleSignInWithEmailAndPassword(
+			authStore.state[EMAIL],
+			authStore.state[PASSWORD]
+		)
+		if (signInFailed) {
+			return { [FORM_ERROR]: signInFailed }
+		}
+		onSignedInRouting(history, lastLocation)
+		userStore.setState({
+			[USER_DISPLAY_NAME]: authStore.state[USERNAME],
+			[USER_EMAIL]: authStore.state[EMAIL],
+			[USER_SIGNED_IN]: true,
+		})
+		// if undefined mean no error
+		// but this not much point since it will redirect and unmount soon
+		return
+	}
 	render() {
 		const {
-			props: { history },
+			props: { history, lastLocation },
+			onSubmit,
 		} = this
 		return (
 			<>
@@ -64,59 +118,77 @@ class LoginPage extends React.Component {
 											<h3 className='mb-0'>Sign in with</h3>
 										</div>
 									</CardBody>
-									<CardBody>
-										<SocialAuthButtonGroup />
-										<Row>
-											<Col />
-											<Col className='text-center text-muted mb-4 mt-3 col-auto'>
-												<small>Or Classically</small>
-											</Col>
-											<Col />
-										</Row>
-										<InputGroup
-											className={classnames('input-lg', {
-												'input-group-focus': this.state.firstNameFocus,
-											})}>
-											<InputGroupAddon addonType='prepend'>
-												<InputGroupText>
-													<i className='tim-icons icon-single-02' />
-												</InputGroupText>
-											</InputGroupAddon>
-											<Input
-												placeholder='First Name...'
-												type='text'
-												onFocus={e => this.setState({ firstNameFocus: true })}
-												onBlur={e => this.setState({ firstNameFocus: false })}
-											/>
-										</InputGroup>
-										<InputGroup
-											className={classnames('input-lg', {
-												'input-group-focus': this.state.lastNameFocus,
-											})}>
-											<InputGroupAddon addonType='prepend'>
-												<InputGroupText>
-													<i className='tim-icons icon-caps-small' />
-												</InputGroupText>
-											</InputGroupAddon>
-											<Input
-												placeholder='Last Name...'
-												type='text'
-												onFocus={e => this.setState({ lastNameFocus: true })}
-												onBlur={e => this.setState({ lastNameFocus: false })}
-											/>
-										</InputGroup>
-									</CardBody>
-									<CardFooter className='text-center'>
-										<Button
-											block
-											className='btn-round'
-											color='primary'
-											href='#pablo'
-											onClick={e => e.preventDefault()}
-											size='lg'>
-											Get Started
-										</Button>
-									</CardFooter>
+									<FinalForm
+										initialValues={{
+											[EMAIL]: '',
+											[PASSWORD]: '',
+										}}
+										decorators={[focusOnError]}
+										onSubmit={values => {
+											return onSubmit(values, history, lastLocation)
+										}}>
+										{({ submitError, handleSubmit, submitting }) => (
+											<>
+												<CardBody>
+													<SocialAuthButtonGroup />
+													<Row>
+														<Col />
+														<Col className='text-center text-muted mb-4 mt-3 col-auto'>
+															<small>Or Classically</small>
+														</Col>
+														<Col />
+													</Row>
+													<InputField
+														type={EMAIL}
+														name={EMAIL}
+														hideSuccess
+														container={authStore}
+														placeholder='Email'
+														icon='tim-icons icon-email-85'
+														validation={value => signInEmailValidation(value)}
+													/>
+													<InputField
+														type={PASSWORD}
+														name={PASSWORD}
+														hideSuccess
+														container={authStore}
+														placeholder='Password'
+														icon='tim-icons icon-lock-circle'
+														validation={value =>
+															signInPasswordValidation(value)
+														}
+													/>
+												</CardBody>
+												<CardFooter className='text-center'>
+													{submitError &&
+														!submitting &&
+														`Error: ${submitError}`}
+													<Button
+														block
+														className='btn-round'
+														color='primary'
+														href='#pablo'
+														size='lg'
+														disabled={submitting}
+														onClick={handleSubmit}>
+														{submitting ? (
+															<>
+																<Loader
+																	type='Watch'
+																	color='#00BFFF'
+																	height='19px'
+																	width='19px'
+																/>
+																&nbsp;&nbsp;Signing In
+															</>
+														) : (
+															'Get Started'
+														)}
+													</Button>
+												</CardFooter>
+											</>
+										)}
+									</FinalForm>
 									<div className='pull-left ml-3 mb-3'>
 										<h6>
 											<a
@@ -135,7 +207,7 @@ class LoginPage extends React.Component {
 												className='link footer-link'
 												href='#pablo'
 												onClick={e => e.preventDefault()}>
-												Need Help?
+												Forgot Password?
 											</a>
 										</h6>
 									</div>
@@ -150,4 +222,4 @@ class LoginPage extends React.Component {
 	}
 }
 
-export default LoginPage
+export default withLastLocation(LoginPage)
