bd3578a1b828b2b98112b3cf87e492817f30c5ae
tylim <limteikyee@gmail.com>
2019-06-16 00:34:32 +0800

    handleIsEmailExist api is no longer necessary

--- functions/src/index.js
+++ functions/src/index.js
@@ -3,11 +3,10 @@
 import * as functions from 'firebase-functions' // https://stackoverflow.com/questions/51118943/cannot-read-property-https-of-undefined-error-in-firebase-functions
 import * as admin from 'firebase-admin'
 import '@babel/polyfill' // https://stackoverflow.com/questions/49253746/error-regeneratorruntime-is-not-defined-with-babel-7
-import { handleSignUpWithEmailAndPassword, handleIsEmailExist } from 'api'
-import { ON_SIGN_UP, ON_IS_EMAIL_EXIST } from 'constantValues'
+import { handleSignUpWithEmailAndPassword } from 'api'
+import { ON_SIGN_UP } from 'constantValues'
 
 admin.initializeApp()
 
 // unable to use property accessor in es6 non default export, revert to es5 exports statement
 exports[ON_SIGN_UP] = functions.https.onCall(handleSignUpWithEmailAndPassword)
-exports[ON_IS_EMAIL_EXIST] = functions.https.onCall(handleIsEmailExist)
