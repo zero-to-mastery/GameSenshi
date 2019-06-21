0c065f14f87bcbf505063d0bf24e3995a2d0a54a
tylim <limteikyee@gmail.com>
2019-06-22 01:18:15 +0800

    add USER_PHONE_NUMBER and USER_PROVIDER_DATA constants

--- src/constantValues/UserConstants.js
+++ src/constantValues/UserConstants.js
@@ -3,15 +3,18 @@
 const USER_UID = 'uid'
 const USER_EMAIL = 'email'
 const USER_PHOTO_URL = 'photoURL'
-const USER_SIGNED_IN = 'SignedIn'
+const USER_SIGNED_IN = 'signedIn'
 const USER_DISPLAY_NAME = 'displayName'
 const USER_EMAIL_IS_VERIFIED = 'emailVerified'
-
+const USER_PHONE_NUMBER = 'phoneNumber'
+const USER_PROVIDER_DATA = 'providerData'
 export {
 	USER_UID,
 	USER_EMAIL,
 	USER_PHOTO_URL,
 	USER_SIGNED_IN,
 	USER_DISPLAY_NAME,
+	USER_PHONE_NUMBER,
+	USER_PROVIDER_DATA,
 	USER_EMAIL_IS_VERIFIED,
 }
--- src/state/UserContainer.js
+++ src/state/UserContainer.js
@@ -6,6 +6,8 @@ import {
 	USER_PHOTO_URL,
 	USER_SIGNED_IN,
 	USER_DISPLAY_NAME,
+	USER_PHONE_NUMBER,
+	USER_PROVIDER_DATA,
 	USER_EMAIL_IS_VERIFIED,
 } from 'constantValues'
 
@@ -15,6 +17,8 @@ const defaultValues = {
 	[USER_PHOTO_URL]: '',
 	[USER_SIGNED_IN]: false,
 	[USER_DISPLAY_NAME]: '',
+	[USER_PHONE_NUMBER]: '',
+	[USER_PROVIDER_DATA]: [],
 	[USER_EMAIL_IS_VERIFIED]: false,
 }
 
