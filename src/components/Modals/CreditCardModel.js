7e08bf7aab2f851804116a5adc1c9b84efec74fa
tylim <limteikyee@gmail.com>
2019-06-25 21:52:27 +0800

    1. card number input no longer rely on key press event
    2. early implementation of react final form

--- src/components/Modals/CreditCardModel.js
+++ src/components/Modals/CreditCardModel.js
@@ -22,6 +22,10 @@ import { userStore, Subscribe } from 'state'
 
 import valid from 'card-validator'
 import { string } from 'yup'
+import { Form as FinalForm } from 'react-final-form'
+import createDecorator from 'final-form-focus'
+
+const focusOnError = createDecorator()
 
 const cardType = cardNumber => {
 	// this is for payment icon
@@ -74,16 +78,14 @@ const CardModal = props => {
 	const [cvc, setCvc] = useState('')
 	const [focus, setFocus] = useState('number')
 	const [checked, setChecked] = useState(false)
-	const [isBackspace, setIsBackspace] = useState(false)
 
 	const { open, toggle } = props
 
+	const onSubmit = () => {}
+
 	const onChangeNumber = e => {
 		let {
-			target: {
-				value,
-				value: { length },
-			},
+			target: { value },
 		} = e
 
 		let validate = false
@@ -96,10 +98,14 @@ const CardModal = props => {
 			//
 		}
 
-		if (validate && value.replace(/ /g, '').length < 17) {
-			if (!isBackspace && [4, 9, 14].includes(length)) {
-				value = value + ' '
-			}
+		value = value.replace(/ /g, '')
+		if (validate && value.length < 17) {
+			const indexes = [4, 9, 14]
+			indexes.forEach(element => {
+				if (value.length > element) {
+					value = value.substr(0, element) + ' ' + value.substr(element)
+				}
+			})
 			setCardNumber(value)
 		}
 	}
@@ -131,181 +137,199 @@ const CardModal = props => {
 							</button>
 							<h5 className='modal-title'>Card</h5>
 						</div>
-						<ModalBody>
-							<Container>
-								<Row>
-									<Col xs='12' md='6'>
-										<FormGroup>
-											<Cards
-												number={cardNumber}
-												name={cardHolderName}
-												expiry={`${expiryMonth}/${expiryYear}`}
-												cvc={cvc}
-												focused={focus}
-											/>
-										</FormGroup>
-									</Col>
-									<Col xs='12' md='6' className='d-flex align-items-center'>
+						<FinalForm
+							initialValues={{
+								cardNumber: '',
+								cvc: '',
+								expiryMonth: '',
+								expiryYear: '',
+								cardHolderName: '',
+							}}
+							decorators={[focusOnError]}
+							onSubmit={values => {
+								return onSubmit(values)
+							}}>
+							{({ handleSubmit, submitting }) => (
+								<>
+									<ModalBody>
 										<Container>
 											<Row>
-												<Col>
+												<Col xs='12' md='6'>
 													<FormGroup>
-														<Input
-															placeholder='Card Number'
-															name='Card Number'
-															type='tel'
-															value={cardNumber}
-															onFocus={() => {
-																setFocus('number')
-															}}
-															onKeyDown={e => {
-																setIsBackspace(e.key === 'Backspace')
-															}}
-															onChange={onChangeNumber}
+														<Cards
+															number={cardNumber}
+															name={cardHolderName}
+															expiry={`${expiryMonth}/${expiryYear}`}
+															cvc={cvc}
+															focused={focus}
 														/>
 													</FormGroup>
 												</Col>
-											</Row>
-											<Row>
-												<Col>
-													<FormGroup>
-														<Input
-															placeholder='Card Holder Name'
-															name='Card Holder Name'
-															type='text'
-															value={cardHolderName}
-															onFocus={() => {
-																setFocus('name')
-															}}
-															onChange={e => {
-																const {
-																	target: { value },
-																} = e
-																if (value.length < 100) {
-																	setCardHolderName(e.target.value)
-																}
-															}}
-														/>
-													</FormGroup>
-												</Col>
-											</Row>
-											<Row>
-												<Col xs='6' className='pr-0'>
-													<FormGroup>
-														<Select
-															className='react-select react-select-info'
-															classNamePrefix='react-select'
-															placeholder='Expiry Month'
-															onFocus={() => {
-																setFocus('expiry')
-															}}
-															value={{
-																value: expiryMonth,
-																label: monthAbvs.find(
-																	monthName => monthName.value === expiryMonth
-																).label,
-															}}
-															onChange={expiryMonth =>
-																setExpiryMonth(expiryMonth.value)
-															}
-															options={monthAbvs}
-														/>
-													</FormGroup>
-												</Col>
-												<Col xs='6'>
-													<FormGroup>
-														<Select
-															className='react-select react-select-info'
-															classNamePrefix='react-select'
-															placeholder='Exp Year'
-															onFocus={() => {
-																setFocus('expiry')
-															}}
-															value={{
-																value: expiryYear,
-																label: years.find(
-																	year => year.value === expiryYear
-																).label,
-															}}
-															onChange={expiryYear =>
-																setExpiryYear(expiryYear.value)
-															}
-															options={years}
-														/>
-													</FormGroup>
-												</Col>
-											</Row>
-											<Row>
-												<Col xs='5'>
-													<FormGroup>
-														<Input
-															placeholder='CVC'
-															name='CVC'
-															type='tel'
-															value={cvc}
-															onFocus={() => {
-																setFocus('cvc')
-															}}
-															onChange={e => {
-																const {
-																	target: { value },
-																} = e
-																if (!isNaN(value) && value.length < 5) {
-																	setCvc(value)
-																}
-															}}
-														/>
-													</FormGroup>
-												</Col>
-												<Col xs='7'>
-													<FormGroup check className='text-left'>
-														<Label check>
-															<Input
-																type='checkbox'
-																checked={checked}
-																onClick={() => {
-																	setChecked(checked => !checked)
-																}}
-																onChange={() => {}}
-															/>
-															<span className='form-check-sign' />
-															set as default?
-														</Label>
-													</FormGroup>
+												<Col
+													xs='12'
+													md='6'
+													className='d-flex align-items-center'>
+													<Container>
+														<Row>
+															<Col>
+																<FormGroup>
+																	<Input
+																		placeholder='Card Number'
+																		name='Card Number'
+																		type='tel'
+																		value={cardNumber}
+																		onFocus={() => {
+																			setFocus('number')
+																		}}
+																		onChange={onChangeNumber}
+																	/>
+																</FormGroup>
+															</Col>
+														</Row>
+														<Row>
+															<Col>
+																<FormGroup>
+																	<Input
+																		placeholder='Card Holder Name'
+																		name='Card Holder Name'
+																		type='text'
+																		value={cardHolderName}
+																		onFocus={() => {
+																			setFocus('name')
+																		}}
+																		onChange={e => {
+																			const {
+																				target: { value },
+																			} = e
+																			if (value.length < 100) {
+																				setCardHolderName(e.target.value)
+																			}
+																		}}
+																	/>
+																</FormGroup>
+															</Col>
+														</Row>
+														<Row>
+															<Col xs='6' className='pr-0'>
+																<FormGroup>
+																	<Select
+																		className='react-select react-select-info'
+																		classNamePrefix='react-select'
+																		placeholder='Expiry Month'
+																		onFocus={() => {
+																			setFocus('expiry')
+																		}}
+																		value={{
+																			value: expiryMonth,
+																			label: monthAbvs.find(
+																				monthName =>
+																					monthName.value === expiryMonth
+																			).label,
+																		}}
+																		onChange={expiryMonth =>
+																			setExpiryMonth(expiryMonth.value)
+																		}
+																		options={monthAbvs}
+																	/>
+																</FormGroup>
+															</Col>
+															<Col xs='6'>
+																<FormGroup>
+																	<Select
+																		className='react-select react-select-info'
+																		classNamePrefix='react-select'
+																		placeholder='Exp Year'
+																		onFocus={() => {
+																			setFocus('expiry')
+																		}}
+																		value={{
+																			value: expiryYear,
+																			label: years.find(
+																				year => year.value === expiryYear
+																			).label,
+																		}}
+																		onChange={expiryYear =>
+																			setExpiryYear(expiryYear.value)
+																		}
+																		options={years}
+																	/>
+																</FormGroup>
+															</Col>
+														</Row>
+														<Row>
+															<Col xs='5'>
+																<FormGroup>
+																	<Input
+																		placeholder='CVC'
+																		name='CVC'
+																		type='tel'
+																		value={cvc}
+																		onFocus={() => {
+																			setFocus('cvc')
+																		}}
+																		onChange={e => {
+																			const {
+																				target: { value },
+																			} = e
+																			if (!isNaN(value) && value.length < 5) {
+																				setCvc(value)
+																			}
+																		}}
+																	/>
+																</FormGroup>
+															</Col>
+															<Col xs='7'>
+																<FormGroup check className='text-left'>
+																	<Label check>
+																		<Input
+																			type='checkbox'
+																			checked={checked}
+																			onClick={() => {
+																				setChecked(checked => !checked)
+																			}}
+																			onChange={() => {}}
+																		/>
+																		<span className='form-check-sign' />
+																		set as default?
+																	</Label>
+																</FormGroup>
+															</Col>
+														</Row>
+													</Container>
 												</Col>
 											</Row>
 										</Container>
-									</Col>
-								</Row>
-							</Container>
-						</ModalBody>
-						<ModalFooter>
-							<Button color='secondary' onClick={toggle}>
-								Close
-							</Button>
-							<Button
-								color='primary'
-								onClick={() => {
-									userStore.setState(state => {
-										if (checked) {
-											state[USER_CARDS].forEach(creditCard => {
-												creditCard.isDefault = false
-											})
-										}
-										state[USER_CARDS].push({
-											last4Digits: cardNumber.slice(-4),
-											expiryYear,
-											expiryMonth,
-											cardType: cardType(cardNumber),
-											isDefault: checked,
-										})
-										toggle()
-										return state
-									})
-								}}>
-								Continue
-							</Button>
-						</ModalFooter>
+									</ModalBody>
+									<ModalFooter>
+										<Button color='secondary' onClick={toggle}>
+											Close
+										</Button>
+										<Button
+											color='primary'
+											onClick={() => {
+												userStore.setState(state => {
+													if (checked) {
+														state[USER_CARDS].forEach(creditCard => {
+															creditCard.isDefault = false
+														})
+													}
+													state[USER_CARDS].push({
+														last4Digits: cardNumber.slice(-4),
+														expiryYear,
+														expiryMonth,
+														cardType: cardType(cardNumber),
+														isDefault: checked,
+													})
+													toggle()
+													return state
+												})
+											}}>
+											Continue
+										</Button>
+									</ModalFooter>
+								</>
+							)}
+						</FinalForm>
 					</Modal>
 				)
 			}}
