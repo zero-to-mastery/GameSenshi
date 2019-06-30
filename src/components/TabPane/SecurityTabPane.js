b38c0f8bd10fad158be335a7daeb1a65632f5a7b
tylim <limteikyee@gmail.com>
2019-06-30 20:43:54 +0800

    continue modification

--- src/components/TabPane/SecurityTabPane.js
+++ src/components/TabPane/SecurityTabPane.js
@@ -1,11 +1,24 @@
-import React from 'react'
-// react plugin used to create switch buttons
-import Switch from 'react-bootstrap-switch'
+import React, { useRef } from 'react'
 
 // reactstrap components
-import { Button, FormGroup, Form, Input, TabPane, Row, Col } from 'reactstrap'
+import { Button, FormGroup, TabPane, Row, Col } from 'reactstrap'
+
+// loader
+import Loader from 'react-loader-spinner'
+
+// form validation
+import { Form as FinalForm } from 'react-final-form'
+import createDecorator from 'final-form-focus'
+import InputField from 'components/Inputs/InputField'
+import { FORM_ERROR } from 'final-form'
+
+// validation
+import { signInPasswordValidation } from 'utils/validation'
+
+const focusOnError = createDecorator()
 
 const SecurityTabPane = props => {
+	const submitButton = useRef(null)
 	return (
 		<TabPane tabId='profile3'>
 			<div className='g-pos-rel h-100 g-brd-around g-brd-gray-light-v7 g-rounded-4 g-pa-15 g-pa-30--md'>
@@ -15,74 +28,68 @@ const SecurityTabPane = props => {
 					</h2>
 				</header>
 				<hr className='line-info' />
-				<Form>
-					<Row>
-						<Col className='align-self-center' md='3'>
-							<label className='labels' htmlFor='#email'>
-								Email
-							</label>
-						</Col>
-						<Col className='align-self-center' md='9'>
-							<FormGroup>
-								<Input
-									defaultValue='charlie.bailey@example.com'
-									id='email'
-									name='email'
-									required='required'
-									type='email'
-								/>
-							</FormGroup>
-						</Col>
-					</Row>
-					<br />
-					<br />
-					<header>
-						<h2 className='text-uppercase'>Security Settings</h2>
-					</header>
-					<hr className='line-info' />
-					<FormGroup className='d-flex align-items-center justify-content-between'>
-						<span>Notify me via email when logging in</span>
-						<Switch offColor='' onColor='' />
-					</FormGroup>
-					<FormGroup className='d-flex align-items-center justify-content-between'>
-						<span>Send SMS confirmation for all online payments</span>
-						<Switch offColor='' onColor='' />
-					</FormGroup>
-					<FormGroup className='d-flex align-items-center justify-content-between'>
-						<span>Check which devices accessed your account</span>
-						<Switch defaultValue={false} offColor='' onColor='' />
-					</FormGroup>
-					<FormGroup className='d-flex align-items-center justify-content-between'>
-						<span>
-							Find My Device, make sure your device can be found if it gets lost
-						</span>
-						<Switch offColor='' onColor='' />
-					</FormGroup>
-					<FormGroup className='d-flex align-items-center justify-content-between'>
-						<span>Lock your device with a PIN, pattern, or password</span>
-						<Switch offColor='' onColor='' />
-					</FormGroup>
-					<FormGroup className='d-flex align-items-center justify-content-between'>
-						<span>
-							Manage what apps have access to app-usage data on your device
-						</span>
-						<Switch defaultValue={false} offColor='' onColor='' />
-					</FormGroup>
-					<Row className='mt-5 justify-content-end'>
-						<Col className='ml-auto' md='4'>
-							<Button
-								className='btn-simple mr-1'
-								color='info'
-								size='sm'
-								type='button'>
-								Cancel
-							</Button>
-							<Button color='info' size='sm' type='button'>
-								Save Changes
-							</Button>
-						</Col>
-					</Row>
-				</Form>
+				<FinalForm
+					initialValues={{
+						email: '',
+						password: '',
+					}}
+					decorators={[focusOnError]}
+					onSubmit={values => {
+						return { [FORM_ERROR]: 'updateFailed' }
+					}}>
+					{({ submitError, handleSubmit, submitting }) => (
+						<>
+							<Row>
+								<Col className='align-self-center' md='3'>
+									<label className='labels' htmlFor='#email'>
+										Email
+									</label>
+								</Col>
+								<Col className='align-self-center' md='9'>
+									<FormGroup>
+										<InputField
+											type='email'
+											name='email'
+											hideSuccess
+											placeholder='email'
+											icon='tim-icons icon-lock-circle'
+											validation={signInPasswordValidation}
+											onKeyPress={e => {
+												e.key === 'Enter' && submitButton.current.onClick()
+											}}
+										/>
+									</FormGroup>
+								</Col>
+							</Row>
+							<Row className='mt-4'>
+								<Col className='d-flex justify-content-center'>
+									<Button
+										color='info'
+										type='button'
+										ref={submitButton}
+										disabled={submitting}
+										onClick={handleSubmit}>
+										{submitting ? (
+											<>
+												<Loader
+													type='Watch'
+													color='#00BFFF'
+													height='19px'
+													width='19px'
+												/>
+												&nbsp;&nbsp;Change Email
+											</>
+										) : (
+											'Get Started'
+										)}
+									</Button>
+								</Col>
+							</Row>
+						</>
+					)}
+				</FinalForm>
+				<br />
+				<br />
 			</div>
 		</TabPane>
 	)
