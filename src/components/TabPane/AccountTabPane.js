d6fcaa567d034a69c0d5d6373bc9f189e805b26a
tylim <limteikyee@gmail.com>
2019-07-04 03:21:11 +0800

    account tab ui done

--- src/components/TabPane/AccountTabPane.js
+++ src/components/TabPane/AccountTabPane.js
@@ -13,7 +13,10 @@ import InputField from 'components/Inputs/InputField'
 import { FORM_ERROR } from 'final-form'
 
 // validation
-import { signInPasswordValidation } from 'utils/validation'
+import {
+	signInEmailValidation,
+	signInPasswordValidation,
+} from 'utils/validation'
 
 const focusOnError = createDecorator()
 
@@ -53,18 +56,19 @@ const AccountTabPane = props => {
 												name='email'
 												hideSuccess
 												placeholder='email'
-												icon='tim-icons icon-lock-circle'
-												validation={signInPasswordValidation}
+												icon='tim-icons icon-email-85'
+												validation={signInEmailValidation}
 												onKeyPress={e => {
 													e.key === 'Enter' && submitButton.current.onClick()
 												}}
 											/>
 										</FormGroup>
 									</Col>
-								</Row>{' '}
+								</Row>
 							</Form>
 							<Row className='mt-4'>
 								<Col className='d-flex justify-content-center'>
+									{submitError && !submitting && `Error: ${submitError}`}
 									<Button
 										color='info'
 										type='button'
@@ -90,8 +94,79 @@ const AccountTabPane = props => {
 						</>
 					)}
 				</FinalForm>
-				<br />
-				<br />
+			</div>
+			<br />
+			<br />
+			<div className='g-pos-rel h-100 g-brd-around g-brd-gray-light-v7 g-rounded-4 g-pa-15 g-pa-30--md'>
+				<header>
+					<h2 className='text-uppercase g-font-size-12 g-font-size-default--md g-color-black mb-0'>
+						Change Password
+					</h2>
+				</header>
+				<hr className='line-info' />
+				<FinalForm
+					initialValues={{
+						email: '',
+						password: '',
+					}}
+					decorators={[focusOnError]}
+					onSubmit={values => {
+						return { [FORM_ERROR]: 'updateFailed' }
+					}}>
+					{({ submitError, handleSubmit, submitting }) => (
+						<>
+							<Form>
+								<Row>
+									<Col className='align-self-center' md='3'>
+										<label className='labels' htmlFor='#email'>
+											Password
+										</label>
+									</Col>
+									<Col className='align-self-center' md='9'>
+										<FormGroup>
+											<InputField
+												type='password'
+												name='password'
+												hideSuccess
+												placeholder='password'
+												icon='tim-icons icon-lock-circle'
+												validation={signInPasswordValidation}
+												onKeyPress={e => {
+													e.key === 'Enter' && submitButton.current.onClick()
+												}}
+											/>
+										</FormGroup>
+									</Col>
+								</Row>{' '}
+							</Form>
+							<Row className='mt-4'>
+								<Col className='d-flex justify-content-center'>
+									{submitError && !submitting && `Error: ${submitError}`}
+									<Button
+										color='info'
+										type='button'
+										ref={submitButton}
+										disabled={submitting}
+										onClick={handleSubmit}>
+										{submitting ? (
+											<>
+												<Loader
+													type='Watch'
+													color='#00BFFF'
+													height='19px'
+													width='19px'
+												/>
+												&nbsp;&nbsp;Change Password
+											</>
+										) : (
+											'Change Password'
+										)}
+									</Button>
+								</Col>
+							</Row>
+						</>
+					)}
+				</FinalForm>
 			</div>
 		</TabPane>
 	)
