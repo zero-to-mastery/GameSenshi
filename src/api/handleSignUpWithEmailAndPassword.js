afd95683d314882a988a4eafba06a88c9408585f
tylim <limteikyee@gmail.com>
2019-07-05 21:45:16 +0800

    cleanup,remove callback

--- src/api/handleSignUpWithEmailAndPassword.js
+++ src/api/handleSignUpWithEmailAndPassword.js
@@ -20,34 +20,13 @@ const defaultValues = {
 	[PASSWORD]: undefined,
 }
 
-const handleSignUpWithEmailAndPassword = (
-	values = defaultValues,
-	routing = () => {}
-) => {
-	//const { [EMAIL]: email, [PASSWORD]: password } = values
-	// return auth()
-	// 	.fetchSignInMethodsForEmail(email)
-	// 	.then(methods => {
-	// 		if (methods.length !== 0 && !methods.includes('password')) {
-	// 			routing()
-	// 			handleDifferentCredential(
-	// 				auth,
-	// 				email,
-	// 				auth.EmailAuthProvider.credential(email, password)
-	// 			)
-	// 			return { [STATUS]: false, ...defaultValues }
-	// 		} else {
+const handleSignUpWithEmailAndPassword = (values = defaultValues) => {
 	return functions
 		.httpsCallable(ON_SIGN_UP)(values)
 		.then(res => {
 			res[DATA][DATA] = { ...defaultValues, ...res[DATA][DATA] }
 			return res[DATA]
 		}) // TODO need FORM_ERROR here
-	// 	}
-	// })
-	// .catch(err => {
-	// 	return 'Unexpected Error Code 1'
-	// })
 }
 
 export default handleSignUpWithEmailAndPassword
