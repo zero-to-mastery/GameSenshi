2c1663499f23af7e14a9f800145f5981e034b541
tylim <limteikyee@gmail.com>
2019-07-10 21:00:16 +0800

    sync

--- src/constantValues/EnvKeyConstants.js
+++ src/constantValues/EnvKeyConstants.js
@@ -1,18 +1,18 @@
-// in future this should turn into npm library so that backend can use the same code
+// firebase do not allow to use any capital letter in env var name
 const ENV = 'env'
-const REACT_APP_ = 'reactApp'
-const REACT_APP_API_KEY = REACT_APP_ + 'ApiKey'
-const REACT_APP_AUTH_DOMAIN = REACT_APP_ + 'AuthDomain'
-const REACT_APP_DATABASE_URL = REACT_APP_ + 'DatabaseURL'
-const REACT_APP_PROJECT_ID = REACT_APP_ + 'ProjectId'
-const REACT_APP_STORAGE_BUCKET = REACT_APP_ + 'StorageBucket'
-const REACT_APP_MESSAGING_SENDER_ID = REACT_APP_ + 'MessagingSenderId'
-const REACT_APP_APP_ID = REACT_APP_ + 'AppId'
-const ENABLE_PLAYGROUND = 'playgroundEnabled'
+const REACT_APP_ = 'react_app_'
+const REACT_APP_API_KEY = REACT_APP_ + 'api_key'
+const REACT_APP_AUTH_DOMAIN = REACT_APP_ + 'auth_domain'
+const REACT_APP_DATABASE_URL = REACT_APP_ + 'database_url'
+const REACT_APP_PROJECT_ID = REACT_APP_ + 'project_id'
+const REACT_APP_STORAGE_BUCKET = REACT_APP_ + 'storage_bucket'
+const REACT_APP_MESSAGING_SENDER_ID = REACT_APP_ + 'messaging_sender_id'
+const REACT_APP_APP_ID = REACT_APP_ + 'app_id'
+const ENABLE_PLAYGROUND = 'playground_enabled'
 
 // third party service
-const VERIFY_EMAIL_API_KEY = 'verifyEmailApiKey'
-const APOLLO_ENGINE_API_KEY = 'apolloEngineApiKey'
+const VERIFY_EMAIL_API_KEY = 'verify_email_api_key'
+const APOLLO_ENGINE_API_KEY = 'apollo_engine_api_key'
 
 export {
 	ENV,
