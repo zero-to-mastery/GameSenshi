f11fba2ca3b6270a33a79f8c88a3d641b25f44c8
tylim <limteikyee@gmail.com>
2019-07-11 17:05:20 +0800

    simplify

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -11,14 +11,16 @@ import 'firebase/storage'
 import { alertStore, authModalStore, userStore, signInStore } from 'state'
 import * as allStore from 'state'
 
+const env = process.env
+
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	databaseURL: process.env.REACT_APP_DATABASE_URL,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_APP_ID,
+	apiKey: env.REACT_APP_API_KEY,
+	authDomain: env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: env.REACT_APP_DATABASE_URL,
+	projectId: env.REACT_APP_PROJECT_ID,
+	storageBucket: env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: env.REACT_APP_APP_ID,
 }
 
 firebase.initializeApp(firebaseConfig)
