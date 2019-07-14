661d46d22184f8adf8f756036b13a91030ebf9d9
tylim <limteikyee@gmail.com>
2019-07-15 02:43:15 +0800

    1. refactor username 2. fix bug that cause tabpane wont change

--- src/views/SettingsPage.jsx
+++ src/views/SettingsPage.jsx
@@ -1,31 +1,27 @@
 import React, { useState, useEffect, useRef, Fragment } from 'react'
+//routing
 import { Route, Switch, Link } from 'react-router-dom'
 // nodejs library that concatenates classes
 import classnames from 'classnames'
-
-//state
-import { Subscribe, userStore } from 'state'
-
 // reactstrap components
 import { NavItem, NavLink, Nav, Container, Row, Col } from 'reactstrap'
-
+// constants
 import {
-	USER_DISPLAY_NAME,
 	ROUTE_PAGE_SETTINGS,
 	ROUTE_PAGE_SETTINGS_BILLING,
 	ROUTE_PAGE_SETTINGS_ACCOUNT,
 	ROUTE_PAGE_SETTINGS_NOTIFICATION,
 } from 'constantValues'
-
 // core components
-import ImageUpload from 'components/CustomUpload/ImageUpload.jsx'
 import {
 	Footer,
 	IndexNavbar,
+	UsernameForm,
 	GeneralSettingsTabPane,
 	BillingSettingsTabPane,
 	AccountSettingsTabPane,
 	NotificationSettingsTabPane,
+	ImageUpload,
 } from 'components'
 
 const routes = [
@@ -63,7 +59,6 @@ const navItems = [
 
 const SettingsPage = props => {
 	const [profileTabs, setProfileTab] = useState(0)
-	const [editMode, setEditMode] = useState(false)
 
 	const wrapper = useRef(null)
 
@@ -98,104 +93,66 @@ const SettingsPage = props => {
 	return (
 		<>
 			<IndexNavbar />
-			<Subscribe to={[userStore]}>
-				{userStore => {
-					const {
-						state: { [USER_DISPLAY_NAME]: name },
-					} = userStore
-					return (
-						<div className='wrapper' ref={wrapper}>
-							<div className='section'>
-								<Container>
-									<Row>
-										<Col md='3'>
-											<div className='section'>
-												{/* User Information */}
-												<section className='text-center'>
-													<ImageUpload avatar addBtnColor='default' />
-													{editMode ? (
-														<p />
-													) : (
-														<h3
-															className='title'
-															onDoubleClick={() => {
-																setEditMode(prev => !prev)
-															}}>
-															{name}{' '}
-															<i
-																className='tim-icons icon-pencil editable'
-																onClick={() => {
-																	setEditMode(prev => !prev)
-																}}
-															/>
-														</h3>
-													)}
-												</section>
-												{/* User Information */}
-												{/* Profile Sidebar */}
-												<section>
-													<br />
-													<Nav
-														className='flex-column nav-tabs-info'
-														role='tablist'>
-														{navItems.map((navItem, i) => {
-															const { navLink, icon, to } = navItem
-															return (
-																<Fragment key={i}>
-																	<NavItem>
-																		<NavLink
-																			className={classnames({
-																				active: profileTabs === i,
-																			})}
-																			onClick={e => setProfileTab(i)}
-																			to={to}
-																			tag={Link}>
-																			<i className={`tim-icons ${icon}`} />{' '}
-																			{navLink}
-																		</NavLink>
-																	</NavItem>
-																	{i + 1 !== navItems.length && (
-																		<hr className='line-info' />
-																	)}
-																</Fragment>
-															)
-														})}
-													</Nav>
-												</section>
-												{/* End Profile Sidebar */}
-												{/* Profile Completion */}
-												<br />
-												<br />
-												<br />
-												{/* End Profile Completion */}
-											</div>
-										</Col>
-										<Col className='ml-auto' md='8'>
-											<div className='section'>
-												<Switch>
-													{routes.map((route, i) => {
-														const { tabPane: TabPane, path } = route
-														return (
-															<Route
-																key={i}
-																path={path}
-																render={props => {
-																	return <TabPane {...props} />
-																}}
-															/>
-														)
-													})}
-												</Switch>
-											</div>
-										</Col>
-									</Row>
-								</Container>
-							</div>
-							<Footer />
-						</div>
-					)
-				}}
-			</Subscribe>
+			<div className='wrapper' ref={wrapper}>
+				<div className='section'>
+					<Container>
+						<Row>
+							<Col md='3'>
+								<div className='section'>
+									{/* User Information */}
+									<section className='text-center'>
+										<ImageUpload avatar addBtnColor='default' />
+										<UsernameForm />
+									</section>
+									{/* User Information */}
+									{/* Profile Sidebar */}
+									<section>
+										<br />
+										<Nav className='flex-column nav-tabs-info' role='tablist'>
+											{navItems.map((navItem, i) => {
+												const { navLink, icon, to } = navItem
+												return (
+													<Fragment key={i}>
+														<NavItem>
+															<NavLink
+																className={classnames({
+																	active: profileTabs === i,
+																})}
+																onClick={e => setProfileTab(i)}
+																to={to}
+																tag={Link}>
+																<i className={`tim-icons ${icon}`} /> {navLink}
+															</NavLink>
+														</NavItem>
+														{i + 1 !== navItems.length && (
+															<hr className='line-info' />
+														)}
+													</Fragment>
+												)
+											})}
+										</Nav>
+									</section>
+									{/* End Profile Sidebar */}
+									{/* Profile Completion */}
+									<br />
+									<br />
+									<br />
+									{/* End Profile Completion */}
+								</div>
+							</Col>
+							<Col className='ml-auto' md='8'>
+								<div className='section'>
+									{profileTabs === 0 && <GeneralSettingsTabPane />}
+									{profileTabs === 1 && <BillingSettingsTabPane />}
+									{profileTabs === 2 && <AccountSettingsTabPane />}
+									{profileTabs === 3 && <NotificationSettingsTabPane />}
+								</div>
+							</Col>
+						</Row>
+					</Container>
+				</div>
+				<Footer />
+			</div>
 		</>
 	)
 }
