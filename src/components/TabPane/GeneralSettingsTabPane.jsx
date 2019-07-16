0f07547813c28a1a50373baa38635f64324cc555
tylim <limteikyee@gmail.com>
2019-07-17 01:25:16 +0800

    add name field

--- src/components/TabPane/GeneralSettingsTabPane.jsx
+++ src/components/TabPane/GeneralSettingsTabPane.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 // react plugin used to create DropdownMenu for selecting items
 import Select from 'react-select'
-
 import { Subscribe, userStore } from 'state'
 // reactstrap components
 import {
@@ -10,20 +9,24 @@ import {
 	Input,
 	Row,
 	Col,
+	Form,
 	UncontrolledPopover,
 } from 'reactstrap'
-
+//constants
 import {
 	USER_UID,
 	USER_GENDER,
 	USER_COUNTRY,
 	USER_LANGUAGES,
 	USER_BIRTH_DATE,
+	USER_DISPLAY_NAME,
 	MONTH_NAMES,
 } from 'constantValues'
-
 // core components
 import Calendar from 'react-calendar'
+import { FinalInput, FinalForm } from 'components'
+// validation
+import { signUpUsernameValidation } from 'utils/validation'
 
 import { getCodes, getNames } from 'country-list'
 import chroma from 'chroma-js'
@@ -132,154 +135,188 @@ const GeneralSettingsTabPane = props => {
 						[USER_BIRTH_DATE]: birthDate,
 						[USER_COUNTRY]: country,
 						[USER_LANGUAGES]: languages,
+						[USER_DISPLAY_NAME]: name,
 					},
 				} = userStore
 				return (
-					<>
-						<div>
-							<header>
-								<h2 className='text-uppercase'>General information</h2>
-							</header>
-							<hr className='line-info' />
-							<br />
-							<Row>
-								<Col className='align-self-center' md='3'>
-									<label className='labels'>UID</label>
-								</Col>
-								<Col className='align-self-center' md='9'>
-									<FormGroup>
-										<label className='labels'>{uid}</label>
-									</FormGroup>
-								</Col>
-							</Row>
-							<Row>
-								<Col className='align-self-center' md='3'>
-									<label className='labels'>I’m</label>
-								</Col>
-								<Col className='align-self-center' md='4'>
-									<FormGroup>
-										<Select
-											className='react-select react-select-info'
-											classNamePrefix='react-select'
-											placeholder='Gender'
-											value={genderOptions.find(
-												genderObj => genderObj.label === gender
-											)}
-											onChange={gender =>
-												userStore.setState({
-													[USER_GENDER]: gender.label,
-												})
-											}
-											options={genderOptions}
-										/>
-									</FormGroup>
-								</Col>
-							</Row>
-							<Row>
-								<Col className='align-self-center' md='3'>
-									<label className='labels' htmlFor='#birthDate'>
-										Birth Date
-									</label>
-								</Col>
-								<Col className='align-self-center' md='4'>
-									<FormGroup>
-										<Input
-											id='birthDate'
-											name='birthDate'
-											type='text'
-											value={`${birthDate.getDate()}-${
-												MONTH_NAMES[birthDate.getMonth() + 1]
-											}-${birthDate.getFullYear()}`}
-											onFocus={() => {
-												setBirthDateOpen(true)
-											}}
-											onChange={() => {}}
-										/>
-										{birthDateOpen && (
-											<UncontrolledPopover
-												trigger='legacy'
-												placement='auto'
-												target='birthDate'>
-												<Calendar
-													activeStartDate={birthDate}
-													value={birthDate}
-													onClickDay={birthDate => {
-														userStore.setState({ [USER_BIRTH_DATE]: birthDate })
-														setBirthDateOpen(false)
-													}}
+					<FinalForm
+						initialValues={{
+							[USER_DISPLAY_NAME]: '',
+						}}
+						onSubmit={values => {
+							//
+						}}>
+						{({ handleSubmit, submitting, submitError, form }) => (
+							<Form className='form'>
+								<div>
+									<header>
+										<h2 className='text-uppercase'>General information</h2>
+									</header>
+									<hr className='line-info' />
+									<br />
+									<Row>
+										<Col className='align-self-center' md='3'>
+											<label className='labels'>UID</label>
+										</Col>
+										<Col className='align-self-center' md='9'>
+											<FormGroup>
+												<label className='labels'>{uid}</label>
+											</FormGroup>
+										</Col>
+									</Row>
+									<Row>
+										<Col className='align-self-center' md='3'>
+											<label className='labels'>Name</label>
+										</Col>
+										<Col
+											className='align-self-center'
+											md='9'
+											style={{ marginBottom: 10 }}>
+											<FinalInput
+												type='username'
+												name={USER_DISPLAY_NAME}
+												hideSuccess
+												onlyShowErrorOnSubmit
+												placeholder={name}
+												validation={value => signUpUsernameValidation(value)}
+												onBlur={() => {
+													form.reset()
+												}}
+											/>
+										</Col>
+									</Row>
+									<Row>
+										<Col className='align-self-center' md='3'>
+											<label className='labels'>I’m</label>
+										</Col>
+										<Col className='align-self-center' md='4'>
+											<FormGroup>
+												<Select
+													className='react-select react-select-info'
+													classNamePrefix='react-select'
+													placeholder='Gender'
+													value={genderOptions.find(
+														genderObj => genderObj.label === gender
+													)}
+													onChange={gender =>
+														userStore.setState({
+															[USER_GENDER]: gender.label,
+														})
+													}
+													options={genderOptions}
 												/>
-											</UncontrolledPopover>
-										)}
-									</FormGroup>
-								</Col>
-							</Row>
-							<Row>
-								<Col className='align-self-center' md='3'>
-									<label className='labels' htmlFor='#location'>
-										Country
-									</label>
-								</Col>
-								<Col className='align-self-center' md='4'>
-									<FormGroup>
-										<Select
-											//styles={customStyles}
-											className='react-select react-select-info'
-											classNamePrefix='react-select'
-											placeholder='Country'
-											value={countryOptions.find(
-												countryObj => countryObj.label === country
-											)}
-											onChange={country =>
-												userStore.setState({
-													[USER_COUNTRY]: country.label,
-												})
-											}
-											options={countryOptions}
-										/>
-									</FormGroup>
-								</Col>
-							</Row>
-							<Row>
-								<Col className='align-self-center' md='3'>
-									<label className='labels'>Language</label>
-								</Col>
-								<Col className='align-self-center' md='9'>
-									<FormGroup>
-										<Select
-											isMulti
-											className='react-select react-select-info'
-											classNamePrefix='react-select'
-											placeholder='Languages'
-											styles={colourStyles}
-											value={languages.reduce((acc, language) => {
-												const found = languageOptions.find(
-													languageObj => languageObj.label === language
-												)
-												acc.push(found)
-												return acc
-											}, [])}
-											onChange={languages =>
-												userStore.setState(state => {
-													state[USER_LANGUAGES] = languages.map(
-														language => language.label
-													)
-													return state
-												})
-											}
-											options={languageOptions}
-										/>
-									</FormGroup>
-								</Col>
-							</Row>
-							<Row className='mt-4'>
-								<Col md='6'>
-									<Button color='info' type='button'>
-										Save Changes
-									</Button>
-								</Col>
-							</Row>
-						</div>
-					</>
+											</FormGroup>
+										</Col>
+									</Row>
+									<Row>
+										<Col className='align-self-center' md='3'>
+											<label className='labels' htmlFor='#birthDate'>
+												Birth Date
+											</label>
+										</Col>
+										<Col className='align-self-center' md='4'>
+											<FormGroup>
+												<Input
+													id='birthDate'
+													name='birthDate'
+													type='text'
+													value={`${birthDate.getDate()}-${
+														MONTH_NAMES[birthDate.getMonth() + 1]
+													}-${birthDate.getFullYear()}`}
+													onFocus={() => {
+														setBirthDateOpen(true)
+													}}
+													onChange={() => {}}
+												/>
+												{birthDateOpen && (
+													<UncontrolledPopover
+														trigger='legacy'
+														placement='auto'
+														target='birthDate'>
+														<Calendar
+															activeStartDate={birthDate}
+															value={birthDate}
+															onClickDay={birthDate => {
+																userStore.setState({
+																	[USER_BIRTH_DATE]: birthDate,
+																})
+																setBirthDateOpen(false)
+															}}
+														/>
+													</UncontrolledPopover>
+												)}
+											</FormGroup>
+										</Col>
+									</Row>
+									<Row>
+										<Col className='align-self-center' md='3'>
+											<label className='labels' htmlFor='#location'>
+												Country
+											</label>
+										</Col>
+										<Col className='align-self-center' md='4'>
+											<FormGroup>
+												<Select
+													//styles={customStyles}
+													className='react-select react-select-info'
+													classNamePrefix='react-select'
+													placeholder='Country'
+													value={countryOptions.find(
+														countryObj => countryObj.label === country
+													)}
+													onChange={country =>
+														userStore.setState({
+															[USER_COUNTRY]: country.label,
+														})
+													}
+													options={countryOptions}
+												/>
+											</FormGroup>
+										</Col>
+									</Row>
+									<Row>
+										<Col className='align-self-center' md='3'>
+											<label className='labels'>Language</label>
+										</Col>
+										<Col className='align-self-center' md='9'>
+											<FormGroup>
+												<Select
+													isMulti
+													className='react-select react-select-info'
+													classNamePrefix='react-select'
+													placeholder='Languages'
+													styles={colourStyles}
+													value={languages.reduce((acc, language) => {
+														const found = languageOptions.find(
+															languageObj => languageObj.label === language
+														)
+														acc.push(found)
+														return acc
+													}, [])}
+													onChange={languages =>
+														userStore.setState(state => {
+															state[USER_LANGUAGES] = languages.map(
+																language => language.label
+															)
+															return state
+														})
+													}
+													options={languageOptions}
+												/>
+											</FormGroup>
+										</Col>
+									</Row>
+									<Row className='mt-4'>
+										<Col md='6'>
+											<Button color='info' type='button'>
+												Save Changes
+											</Button>
+										</Col>
+									</Row>
+								</div>
+							</Form>
+						)}
+					</FinalForm>
 				)
 			}}
 		</Subscribe>
