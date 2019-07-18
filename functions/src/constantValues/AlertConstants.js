6ba897f8cf4c9dfdb4ce45f2c1de7c512c91d12a
tylim <limteikyee@gmail.com>
2019-07-18 17:28:47 +0800

    sync

--- functions/src/constantValues/AlertConstants.js
+++ functions/src/constantValues/AlertConstants.js
@@ -2,5 +2,6 @@ const ALERT = 'alert'
 const ALERT_BODY = ALERT + 'Body'
 const ALERT_OPEN = ALERT + 'Open'
 const ALERT_COLOR = ALERT + 'Color'
+const ALERT_ICON = ALERT + 'Icon'
 
-export { ALERT_BODY, ALERT_COLOR, ALERT_OPEN }
+export { ALERT_BODY, ALERT_COLOR, ALERT_OPEN, ALERT_ICON }
