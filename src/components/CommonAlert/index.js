2500f5c5f1875609e90eb47fc1950790410fd731
tylim <limteikyee@gmail.com>
2019-07-21 12:43:12 +0800

    add comment

--- src/components/CommonAlert/index.js
+++ src/components/CommonAlert/index.js
@@ -7,6 +7,9 @@ import {
 	COMMON_ALERT_COLOR,
 } from 'components/CommonAlert/CommonAlert'
 
+// check container component props name before you map your props in it
+// skip unnecessary by passing random string, for example
+// [	COMMON_ALERT_BODY, "unusedProps1" ,COMMON_ALERT_ICON,]
 const CommonAlertContainerAlert = AlertContainer(CommonAlert, alertStore, [
 	COMMON_ALERT_BODY,
 	COMMON_ALERT_ICON,
