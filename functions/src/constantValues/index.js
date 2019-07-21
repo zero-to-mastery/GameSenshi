0b3ec6c98849599a4b9834cd37f87adc2ff0112a
tylim <limteikyee@gmail.com>
2019-07-22 00:58:33 +0800

    sync

--- functions/src/constantValues/index.js
+++ functions/src/constantValues/index.js
@@ -126,12 +126,6 @@ import {
 	ROUTE_PAGE_PASSWORD_RESET,
 } from 'constantValues/RouteConstants'
 
-import {
-	PROGRESS_OPEN,
-	PROGRESS_VALUE,
-	PROGRESS_COLOR,
-} from 'constantValues/ProgressConstant'
-
 import { FIREBASE_STORAGE_USER_AVATAR } from 'constantValues/FirebaseConstants'
 
 export {
@@ -230,8 +224,5 @@ export {
 	ROUTE_PAGE_SETTINGS_ACCOUNT,
 	ROUTE_PAGE_SETTINGS_NOTIFICATION,
 	ROUTE_PAGE_PASSWORD_RESET,
-	PROGRESS_OPEN,
-	PROGRESS_VALUE,
-	PROGRESS_COLOR,
 	FIREBASE_STORAGE_USER_AVATAR,
 }
