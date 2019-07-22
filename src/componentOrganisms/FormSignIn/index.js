b735e5f03444ccec651eeab807940faf035a5ff2
tylim <limteikyee@gmail.com>
2019-07-23 04:00:20 +0800

    create FormSignIn with injected staple props

--- src/componentOrganisms/FormSignIn/index.js
+++ src/componentOrganisms/FormSignIn/index.js
@@ -1,11 +1,23 @@
+import React from 'react'
+import { withLastLocation } from 'react-router-last-location'
+// validation
 import {
-	FormSignIn,
+	signInEmailValidation,
+	signInPasswordValidation,
+} from 'utils/validation'
+// constants
+import { ROUTE_PAGE_PASSWORD_RESET, onSignedInRouting } from 'routes'
+// api
+import { handleSignInWithEmailAndPassword } from 'api'
+// component
+import {
+	FormSignIn as FormSignInPure,
 	SIGN_IN_FROM_STATE_EMAIL,
 	SIGN_IN_FROM_STATE_IS_OPEN,
 	SIGN_IN_FROM_TOGGLE,
 	SIGN_IN_FROM_ON_SUCCESSFUL_SUBMISSION,
-} from 'componentOrganisms/FormSignIn'
-
+} from 'componentOrganisms/FormSignIn/FormSignIn'
+// store
 import {
 	signInStore,
 	StateContainer,
@@ -14,6 +26,23 @@ import {
 	SIGN_IN_STORE_STATE_EMAIL,
 	SIGN_IN_STORE_STATE_IS_OPEN,
 } from 'state'
+
+// inject staple props that suitable for this app
+const FormSignIn = withLastLocation(props => {
+	const { history, lastLocation } = props
+	return (
+		<FormSignInPure
+			emailValidation={signInEmailValidation}
+			passwordValidation={signInPasswordValidation}
+			forgotPasswordLink={ROUTE_PAGE_PASSWORD_RESET}
+			onSubmit={handleSignInWithEmailAndPassword}
+			onSuccessfulSubmission={() => {
+				onSignedInRouting(history, lastLocation)
+			}}
+			{...props}
+		/>
+	)
+})
 
 const signInStoreStateToPropsMap = {
 	[SIGN_IN_FROM_STATE_EMAIL]: SIGN_IN_STORE_STATE_EMAIL,
@@ -25,11 +54,11 @@ const signInStoreMethodToPropsMap = {
 	[SIGN_IN_FROM_ON_SUCCESSFUL_SUBMISSION]: signInStoreOnSuccessfulSubmission,
 }
 
-const FormSignInStoreAlert = StateContainer(
+const FormSignInStoreSignIn = StateContainer(
 	FormSignIn,
 	signInStore,
 	signInStoreStateToPropsMap,
 	signInStoreMethodToPropsMap
 )
 
-export { FormSignIn, FormSignInStoreAlert }
+export { FormSignIn, FormSignInStoreSignIn }
