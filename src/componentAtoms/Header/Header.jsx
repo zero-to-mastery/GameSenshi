4cf52aa1093e0830b3cc4c50d856a6187c6f3d9f
tylim <limteikyee@gmail.com>
2019-08-04 15:09:58 +0800

    new component Header

---
+++ src/componentAtoms/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+const Headers = props => {
+	const { children } = props
+	return (
+		<>
+			<header>
+				<h2 className='text-uppercase g-font-size-12 g-font-size-default--md g-color-black mb-0'>
+					{children}
+				</h2>
+			</header>
+			<hr className='line-info' />
+		</>
+	)
+}
+
+export { Headers }
---
+++ src/componentAtoms/Header/index.js
@@ -0,0 +1,3 @@
+import { Header } from ' componentAtoms/Header'
+
+export { Header }
