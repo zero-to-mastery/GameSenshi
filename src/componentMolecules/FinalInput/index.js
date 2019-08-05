627ef5247421423622bf429e28b16d662704c9b3
tylim <limteikyee@gmail.com>
2019-08-05 15:52:15 +0800

    use render prop instead of higher order component to solve Unknown name issue

--- src/componentMolecules/FinalInput/index.js
+++ src/componentMolecules/FinalInput/index.js
@@ -1,10 +1,16 @@
+import React from 'react'
 import { FinalInput } from 'componentMolecules/FinalInput/FinalInput'
 import { ExportAtoms } from 'componentAtoms'
 import { stopUndefined } from 'utils'
 
 const { InputText, InputSelect } = stopUndefined(ExportAtoms)
 
-const FinalInputText = FinalInput(InputText)
-const FinalInputSelect = FinalInput(InputSelect)
+const FinalInputText = props => {
+	return <FinalInput {...props} Component={InputText} />
+}
+
+const FinalInputSelect = props => {
+	return <FinalInput {...props} Component={InputSelect} />
+}
 
 export { FinalInputText, FinalInputSelect }
