c72250ed09923d9e44709f048b6f135d67b08ff7
tylim <limteikyee@gmail.com>
2019-08-07 19:19:51 +0800

    no longer display empty value if value is decided by parent component

--- src/componentAtoms/InputSelect/InputSelect.jsx
+++ src/componentAtoms/InputSelect/InputSelect.jsx
@@ -3,12 +3,13 @@ import Select from 'react-select'
 import { FormGroup } from 'reactstrap'
 
 const InputSelect = props => {
-	const { className, classNamePrefix, children, ...restProps } = props
+	const { className, classNamePrefix, value, children, ...restProps } = props
 	return (
 		<FormGroup>
 			<Select
 				className={className || 'react-select react-select-info'}
 				classNamePrefix={classNamePrefix || 'react-select'}
+				value={value.value || props.options[0]}
 				{...restProps}
 			/>
 			{children}
