7b8d4fb0d5d397a68e3c97e9a482f7cdbd67abd2
tylim <limteikyee@gmail.com>
2019-08-23 22:53:24 +0800

    select country component

---
+++ src/componentnCompounds/FinalSelectCountry/FinalSelectCountry.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { stopUndefined } from 'utils'
+import { getCodes, getNames } from 'country-list'
+//components
+import { ExportMolecules } from 'componentMolecules'
+
+const { FinalInputSelect, VALUE, LABEL, IS_DISABLED } = stopUndefined(
+	ExportMolecules
+)
+const FINAL_SELECT_COUNTRY = 'expiryMonth'
+
+const countries = getNames()
+const countryCodes = getCodes()
+
+const countriesAndCodes = countries.reduce((acc, country, i) => {
+	acc.push({ [VALUE]: countryCodes[i], [LABEL]: country })
+	return acc
+}, [])
+
+const countryOptions = [
+	{
+		[VALUE]: '',
+		[LABEL]: 'Country',
+		[IS_DISABLED]: true,
+	},
+	...countriesAndCodes,
+]
+
+const FinalSelectCountry = props => {
+	const { validation, ...restProps } = props
+	return (
+		<FinalInputSelect
+			name={FINAL_SELECT_COUNTRY}
+			placeholder='Country'
+			hideSuccess
+			validation={value => validation(value, countryOptions)}
+			options={countryOptions}
+			defaultValue={countryOptions[0]}
+			{...restProps}
+		/>
+	)
+}
+
+export { FinalSelectCountry, FINAL_SELECT_COUNTRY }
---
+++ src/componentnCompounds/FinalSelectCountry/index.js
@@ -0,0 +1,6 @@
+import {
+	FinalSelectCountry,
+	FINAL_SELECT_COUNTRY,
+} from 'componentnCompounds/FinalSelectCountry/FinalSelectCountry'
+
+export { FinalSelectCountry, FINAL_SELECT_COUNTRY }
