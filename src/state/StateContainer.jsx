a3ab374924ea99cab1301def50507849a21d96ca
tylim <limteikyee@gmail.com>
2019-09-03 01:14:05 +0800

    fix bug that cause all state changed not detected

--- src/state/StateContainer.jsx
+++ src/state/StateContainer.jsx
@@ -8,18 +8,18 @@ const StateContainer = (
 	stateToPropsMaps = [],
 	methodToPropsMaps = []
 ) => {
-	const accProps = stores.reduce((accProps, store, i) => {
-		for (const prop in stateToPropsMaps[i]) {
-			accProps[prop] = store[STATE][stateToPropsMaps[i][prop]]
-		}
-		for (const prop in methodToPropsMaps[i]) {
-			accProps[prop] = methodToPropsMaps[i][prop]
-		}
-		return accProps
-	}, {})
 	return props => (
 		<Subscribe to={stores}>
 			{() => {
+				const accProps = stores.reduce((accProps, store, i) => {
+					for (const prop in stateToPropsMaps[i]) {
+						accProps[prop] = store[STATE][stateToPropsMaps[i][prop]]
+					}
+					for (const prop in methodToPropsMaps[i]) {
+						accProps[prop] = methodToPropsMaps[i][prop]
+					}
+					return accProps
+				}, {})
 				return <Component {...accProps} {...props} />
 			}}
 		</Subscribe>
