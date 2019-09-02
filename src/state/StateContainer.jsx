1108a60cf1152aaf5015ff6d0d95016f091d0139
tylim <limteikyee@gmail.com>
2019-09-02 22:23:49 +0800

    simplify and optimize

--- src/state/StateContainer.jsx
+++ src/state/StateContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Subscribe } from 'unstated'
+import { STATE } from 'state/constants'
 
 const StateContainer = (
 	Component,
@@ -7,22 +8,19 @@ const StateContainer = (
 	stateToPropsMaps = [],
 	methodToPropsMaps = []
 ) => {
+	const accProps = stores.reduce((accProps, store, i) => {
+		for (const prop in stateToPropsMaps[i]) {
+			accProps[prop] = store[STATE][stateToPropsMaps[i][prop]]
+		}
+		for (const prop in methodToPropsMaps[i]) {
+			accProps[prop] = methodToPropsMaps[i][prop]
+		}
+		return accProps
+	}, {})
 	return props => (
 		<Subscribe to={stores}>
 			{() => {
-				const accProps = stores.reduce((accProps, store, i) => {
-					for (const prop in stateToPropsMaps[i]) {
-						accProps[prop] = store.state[stateToPropsMaps[i][prop]]
-					}
-					for (const prop in methodToPropsMaps[i]) {
-						accProps[prop] = () => {
-							props[prop] && props[prop]()
-							methodToPropsMaps[i][prop]()
-						}
-					}
-					return accProps
-				}, {})
-				return <Component {...props} {...accProps} />
+				return <Component {...accProps} {...props} />
 			}}
 		</Subscribe>
 	)
