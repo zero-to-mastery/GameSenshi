543bc11baa2b2594bae20914e83e884222979471
tylim <limteikyee@gmail.com>
2019-09-03 20:30:55 +0800

    revert

--- src/api/handleSignInWithFacebook.js
+++ src/api/handleSignInWithFacebook.js
@@ -1,13 +1,9 @@
 import { auth } from 'firebaseInit'
-import { simplerResponseHandling } from 'utils'
-import { UNEXPECTED_ERROR_CODE_6 } from 'constantValues'
 
 const provider = new auth.FacebookAuthProvider()
 
 const handleSignInWithFacebook = () => {
-	auth()
-		.signInWithRedirect(provider)
-		.catch(err => simplerResponseHandling(false, UNEXPECTED_ERROR_CODE_6, err))
+	auth().signInWithRedirect(provider)
 }
 
 export default handleSignInWithFacebook
--- src/api/handleSignInWithGoogle.js
+++ src/api/handleSignInWithGoogle.js
@@ -1,13 +1,9 @@
 import { auth } from 'firebaseInit'
-import { simplerResponseHandling } from 'utils'
-import { UNEXPECTED_ERROR_CODE_7 } from 'constantValues'
 
 const provider = new auth.GoogleAuthProvider()
 
 const handleSignInWithGoogle = () => {
-	auth()
-		.signInWithRedirect(provider)
-		.catch(err => simplerResponseHandling(false, UNEXPECTED_ERROR_CODE_7, err))
+	auth().signInWithRedirect(provider)
 }
 
 export default handleSignInWithGoogle
