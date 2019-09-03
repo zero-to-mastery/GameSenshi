b947c91b14d63e9ce49db2c28ae18012b22f2f75
tylim <limteikyee@gmail.com>
2019-09-03 15:13:04 +0800

    standardize after onSubmit process

--- src/componentAtoms/FinalForm/FinalForm.jsx
+++ src/componentAtoms/FinalForm/FinalForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Form } from 'react-final-form'
 import { FORM_ERROR } from 'final-form'
 import createDecorator from 'final-form-focus'
@@ -6,15 +6,23 @@ import createDecorator from 'final-form-focus'
 const focusOnError = createDecorator()
 
 const FinalForm = props => {
-	const { onSubmit, ...otherProps } = props
+	const { onSubmit, onSuccessfulSubmission, ...otherProps } = props
+	const onSubmit_ = useCallback(async values => {
+		const response = await onSubmit(values)
+		const { status, data, message } = response
+
+		if (status) {
+			onSuccessfulSubmission(values)
+			return undefined
+		} else if (status === false) {
+			const formError = { [FORM_ERROR]: message }
+			return data ? { ...data, ...formError } : formError
+		} else {
+			return 'Unknown Unhandled Exception'
+		}
+	})
 	return (
-		<Form
-			onSubmit={(...args) => {
-				return onSubmit(FORM_ERROR, ...args)
-			}}
-			decorators={[focusOnError]}
-			{...otherProps}
-		/>
+		<Form onSubmit={onSubmit_} decorators={[focusOnError]} {...otherProps} />
 	)
 }
 
