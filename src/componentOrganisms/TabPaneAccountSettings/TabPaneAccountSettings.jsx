899ee610001f8b47b306c468fd24bcf522b6ce4e
tylim <limteikyee@gmail.com>
2019-09-07 23:56:17 +0800

    optimize

--- src/componentOrganisms/TabPaneAccountSettings/TabPaneAccountSettings.jsx
+++ src/componentOrganisms/TabPaneAccountSettings/TabPaneAccountSettings.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useCallback } from 'react'
 import { stopUndefined } from 'utils'
 // reactstrap components
 import { Row, Col, Form } from 'reactstrap'
@@ -11,12 +11,19 @@ const {
 	HeaderLined,
 	TabPaneContainer,
 	FinalTextEmailPropedAccount,
-	FinalTextPasswordPropedAccount,
 	FINAL_TEXT_EMAIL,
+	FinalTextPasswordPropedAccount,
 	FINAL_TEXT_PASSWORD,
 } = stopUndefined(ExportCompounds)
 
 const TabPaneAccountSettings = props => {
+	const {
+		onSubmitEmail,
+		onSuccessfulSubmissionEmail,
+		onSubmitPassword,
+		onSuccessfulSubmissionPassword,
+	} = props
+
 	const submitButtonEmail = useRef(null)
 	const submitButtonPassWord = useRef(null)
 	return (
@@ -27,32 +34,34 @@ const TabPaneAccountSettings = props => {
 					initialValues={{
 						[FINAL_TEXT_EMAIL]: '',
 					}}
-					onSubmit={(formError, values) => {
-						return { [formError]: 'updateFailed' }
-					}}>
-					{({ submitError, handleSubmit, submitting, form }) => (
-						<Form>
-							<FinalTextEmailPropedAccount
-								submitRef={submitButtonEmail}
-								onBlur={() => {
-									form.reset()
-								}}
-							/>
-							<Row className='mt-4'>
-								<Col className='d-flex justify-content-center'>
-									{submitError && !submitting && `Error: ${submitError}`}
-									<ButtonSubmit
-										color='info'
-										type='button'
-										submitRef={submitButtonEmail}
-										disabled={submitting}
-										onClick={handleSubmit}>
-										{submitting ? 'Changing Email' : 'Change Email'}
-									</ButtonSubmit>
-								</Col>
-							</Row>
-						</Form>
-					)}
+					onSubmit={onSubmitEmail}
+					onSuccessfulSubmission={onSuccessfulSubmissionEmail}>
+					{({ submitError, handleSubmit, submitting, form }) => {
+						const formReset = useCallback(() => {
+							form.reset()
+						}, [])
+						return (
+							<Form>
+								<FinalTextEmailPropedAccount
+									submitRef={submitButtonEmail}
+									onBlur={formReset}
+								/>
+								<Row className='mt-4'>
+									<Col className='d-flex justify-content-center'>
+										{submitError && !submitting && `Error: ${submitError}`}
+										<ButtonSubmit
+											color='info'
+											type='button'
+											submitRef={submitButtonEmail}
+											disabled={submitting}
+											onClick={handleSubmit}>
+											{submitting ? 'Changing Email' : 'Change Email'}
+										</ButtonSubmit>
+									</Col>
+								</Row>
+							</Form>
+						)
+					}}
 				</FinalForm>
 			</TabPaneContainer>
 			<hr />
@@ -63,34 +72,35 @@ const TabPaneAccountSettings = props => {
 					initialValues={{
 						[FINAL_TEXT_PASSWORD]: '',
 					}}
-					onSubmit={(formError, values) => {
-						return { [formError]: 'updateFailed' }
-					}}>
-					{({ submitError, handleSubmit, submitting, form }) => (
-						<>
+					onSubmit={onSubmitPassword}
+					onSuccessfulSubmission={onSuccessfulSubmissionPassword}>
+					>
+					{({ submitError, handleSubmit, submitting, form }) => {
+						const formReset = useCallback(() => {
+							form.reset()
+						}, [])
+						return (
 							<Form>
 								<FinalTextPasswordPropedAccount
 									submitRef={submitButtonPassWord}
-									onBlur={() => {
-										form.reset()
-									}}
+									onBlur={formReset}
 								/>
+								<Row className='mt-4'>
+									<Col className='d-flex justify-content-center'>
+										{submitError && !submitting && `Error: ${submitError}`}
+										<ButtonSubmit
+											color='info'
+											type='button'
+											submitRef={submitButtonPassWord}
+											disabled={submitting}
+											onClick={handleSubmit}>
+											{submitting ? 'Changing Password' : 'Change Password'}
+										</ButtonSubmit>
+									</Col>
+								</Row>
 							</Form>
-							<Row className='mt-4'>
-								<Col className='d-flex justify-content-center'>
-									{submitError && !submitting && `Error: ${submitError}`}
-									<ButtonSubmit
-										color='info'
-										type='button'
-										submitRef={submitButtonPassWord}
-										disabled={submitting}
-										onClick={handleSubmit}>
-										{submitting ? 'Changing Password' : 'Change Password'}
-									</ButtonSubmit>
-								</Col>
-							</Row>
-						</>
-					)}
+						)
+					}}
 				</FinalForm>
 			</TabPaneContainer>
 		</>
