72ff12994023ec3d400e64c972b94519317d1667
tylim <limteikyee@gmail.com>
2019-10-02 00:41:49 +0800

    add loading props to checkbox

--- src/componentAtoms/CheckBox/Checkbox.jsx
+++ src/componentAtoms/CheckBox/Checkbox.jsx
@@ -1,12 +1,19 @@
 import React, { useState, useCallback } from 'react'
-
-// reactstrap components
+import Loader from 'react-loader-spinner'
 import { Label, FormGroup, Input } from 'reactstrap'
 
 const CheckBox = props => {
-	const { initialValue, onChange, className, children, ...otherProps } = props
+	const {
+		initialValue,
+		onChange,
+		className,
+		children,
+		loading,
+		...otherProps
+	} = props
 
 	const [value, setValue] = useState(initialValue)
+	//const [loading, setLoading] = useState(false)
 	const onChange_ = useCallback(
 		e => {
 			setValue(value => !value)
@@ -14,17 +21,38 @@ const CheckBox = props => {
 		},
 		[onChange]
 	)
+
 	return (
-		<FormGroup check className={className}>
+		<FormGroup check={!loading} className={`mb-0 ${className}`}>
 			<Label check>
-				<Input
-					type='checkbox'
-					value={value}
-					onChange={onChange_}
-					{...otherProps}
-				/>
-				<span className='form-check-sign' />
-				{children}
+				{!loading && (
+					<Input
+						type='checkbox'
+						value={value}
+						onChange={onChange_}
+						{...otherProps}
+					/>
+				)}
+				<div className='form-check-sign d-flex'>
+					{loading && (
+						<>
+							<Loader
+								type='Circles'
+								color='#00BFFF'
+								height='18px'
+								width='18px'
+							/>
+							<span style={{ marginRight: '7px' }} />
+						</>
+					)}
+					<span
+						/* onClick={() => {
+							setLoading(state => !state)
+						}} */
+						style={{ color: 'rgb(255,255,255,0.6' }}>
+						{children}
+					</span>
+				</div>
 			</Label>
 		</FormGroup>
 	)
