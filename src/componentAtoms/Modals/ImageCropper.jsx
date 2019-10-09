3ce27385a210be9cd632ecec796cf3a3ebb22925
kam773 <kamilsmas@gmail.com>
2019-10-09 16:22:17 +0200

    add reactstrap modal to ImageCropper component

--- src/componentAtoms/Modals/ImageCropper.jsx
+++ src/componentAtoms/Modals/ImageCropper.jsx
@@ -1,21 +1,32 @@
 import React, { useState, useRef } from 'react'
 import Cropper from 'react-cropper'
 import 'cropperjs/dist/cropper.css'
+// components
+import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap'
+
 const ImageCropper = props => {
 	const cropperRef = useRef(null)
+
 	const onCropping = () => {
 		console.log(cropperRef.current.getCroppedCanvas().toDataURL())
 	}
 	return (
-		<Cropper
-			ref={cropperRef}
-			src='http://fengyuanchen.github.io/cropper/img/picture.jpg'
-			style={{ height: 400, width: '100%' }}
-			// Cropper.js options
-			aspectRatio={16 / 9}
-			guides={false}
-			crop={onCropping}
-		/>
+		<Modal>
+			<ModalBody>
+				<Cropper
+					ref={cropperRef}
+					src='http://fengyuanchen.github.io/cropper/img/picture.jpg'
+					style={{ height: 400, width: '100%' }}
+					// Cropper.js options
+					aspectRatio={16 / 9}
+					guides={false}
+					crop={onCropping}
+				/>
+			</ModalBody>
+			<ModalFooter>
+				<Button color='primary'>Set new profile picture</Button>{' '}
+			</ModalFooter>
+		</Modal>
 	)
 }
 
