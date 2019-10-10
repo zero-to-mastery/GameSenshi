4d4f7c65e483153bada1ab22a19414fd6cbced6d
tylim <limteikyee@gmail.com>
2019-10-11 01:26:08 +0800

    change paramter name and add more paramater

--- functions/src/firebaseInit/onUserCreate.js
+++ functions/src/firebaseInit/onUserCreate.js
@@ -13,18 +13,20 @@ import {
 	FB_FS_SETTINGS_GENERAL_DISPLAY_NAME,
 } from 'constantValues'
 
-const onUserCreate = (user, firestore) => {
-	const isPasswordExist = user.providerData.some(
+const onUserCreate = (userRecord, eventContext, firestore) => {
+	console.log(userRecord, eventContext)
+	const isPasswordExist = userRecord.providerData.some(
 		data => data.providerId === 'password'
 	)
 
 	if (!isPasswordExist) {
-		firestore.doc(fbfsSettingsGeneral(user)).set({
-			[FB_FS_SETTINGS_GENERAL_DISPLAY_NAME]: user.displayName || user.uid,
+		firestore.doc(fbfsSettingsGeneral(userRecord)).set({
+			[FB_FS_SETTINGS_GENERAL_DISPLAY_NAME]:
+				userRecord.displayName || userRecord.uid,
 		})
 	}
 
-	return firestore.doc(fbfsSettingsNotification(user)).set({
+	return firestore.doc(fbfsSettingsNotification(userRecord)).set({
 		[FB_FS_SETTINGS_NOTIFICATION_EMAIL]: {
 			[FB_FS_SETTINGS_NOTIFICATION_EMAIL_ORDER_UPDATES]: true,
 			[FB_FS_SETTINGS_NOTIFICATION_EMAIL_CHATS]: true,
