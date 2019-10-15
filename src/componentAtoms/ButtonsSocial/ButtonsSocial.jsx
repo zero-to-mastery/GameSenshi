d420da287dce5c8a5f551f0faa489f999672af86
kam773 <kamilsmas@gmail.com>
2019-10-15 17:13:37 +0200

    encapsulate non-presentational part

--- src/componentAtoms/ButtonsSocial/ButtonsSocial.jsx
+++ src/componentAtoms/ButtonsSocial/ButtonsSocial.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import { Button, UncontrolledTooltip } from 'reactstrap'
+
+const BUTTONS_SOCIAL_AUTH_ON_CLICKS = 'onClicks'
+const ID = 'id'
+const ICON = 'icon'
+const COLOR = 'color'
+const TOOLTIP = 'tooltip'
+const BUTTONS = 'buttons'
 
 const ButtonsSocial = props => {
 	return <div>hello</div>
--- src/componentAtoms/ButtonsSocial/index.js
+++ src/componentAtoms/ButtonsSocial/index.js
@@ -1,2 +1,29 @@
-import { ButtonsSocial } from './ButtonsSocial'
-export { ButtonsSocial }
+import React from 'react'
+import { ButtonsSocial, ID, COLOR, ICON, TOOLTIP } from './ButtonsSocial'
+
+const buttons = [
+	{
+		[ID]: 'facebook',
+		[ICON]: 'fab fa-facebook',
+		[COLOR]: '#3b5998',
+		[TOOLTIP]: 'Connect via Facebook',
+	},
+	{
+		[ID]: 'twitch',
+		[ICON]: 'fab fa-twitch',
+		[COLOR]: '#6441a5',
+		[TOOLTIP]: 'Connect via Twitch',
+	},
+	{
+		[ID]: 'youtube',
+		[ICON]: 'fab fa-youtube',
+		[COLOR]: '#c4302b',
+		[TOOLTIP]: 'Connect via Youtube',
+	},
+]
+
+const ButtonsSocialPropedDefault = props => {
+	return <ButtonsSocial buttons={buttons} />
+}
+
+export { ButtonsSocial, ButtonsSocialPropedDefault }
