7187c67fda3e2e1eeefb1e8bd053df161e92d296
kam773 <kamilsmas@gmail.com>
2019-10-15 17:23:04 +0200

    add buttons to proesentational component

--- src/componentAtoms/ButtonsSocial/ButtonsSocial.jsx
+++ src/componentAtoms/ButtonsSocial/ButtonsSocial.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Button, UncontrolledTooltip } from 'reactstrap'
 
-const BUTTONS_SOCIAL_AUTH_ON_CLICKS = 'onClicks'
+const BUTTONS_SOCIAL_PROFILE = 'onClicks'
 const ID = 'id'
 const ICON = 'icon'
 const COLOR = 'color'
@@ -9,7 +9,37 @@ const TOOLTIP = 'tooltip'
 const BUTTONS = 'buttons'
 
 const ButtonsSocial = props => {
-	return <div>hello</div>
+	const { [BUTTONS]: buttons, [BUTTONS_SOCIAL_PROFILE]: onClicks } = props
+
+	return (
+		<div className='btn-wrapper text-center'>
+			{buttons.map((button, i) => {
+				const {
+					[ID]: id,
+					[ICON]: icon,
+					[COLOR]: color,
+					[TOOLTIP]: tooltip,
+				} = button
+				return (
+					<Fragment key={id}>
+						<Button
+							size='lg'
+							className='btn-icon btn-round'
+							color={color}
+							href=''
+							id={id}
+							target='_blank'
+							onClick={onClicks[i]}>
+							<i className={icon} />
+						</Button>
+						<UncontrolledTooltip delay={0} target={id}>
+							{tooltip}
+						</UncontrolledTooltip>
+					</Fragment>
+				)
+			})}
+		</div>
+	)
 }
 
 export { ButtonsSocial }
