79dfd50f95d70435f5a70cf16e494546923fe079
kam773 <kamilsmas@gmail.com>
2019-10-20 14:55:33 +0200

    remove commented code

--- src/componentAtoms/PaginationCommon/PaginationCommon.jsx
+++ src/componentAtoms/PaginationCommon/PaginationCommon.jsx
@@ -13,47 +13,6 @@ const PaginationCommon = props => {
 		items.current = props.items
 	})
 
-	// const getPager = useCallback(
-	// 	(totalItems, currentPage, pageSize) => {
-	// 		currentPage = currentPage || 1
-	// 		pageSize = pageSize || 3
-	// 		const totalPages = Math.ceil(totalItems / pageSize)
-	// 		let startPage, endPage
-	// 		if (totalPages <= 10) {
-	// 			startPage = 1
-	// 			endPage = totalPages
-	// 		} else {
-	// 			if (currentPage <= 6) {
-	// 				startPage = 1
-	// 				endPage = 10
-	// 			} else if (currentPage + 4 >= totalPages) {
-	// 				startPage = totalPages - 9
-	// 				endPage = totalPages
-	// 			} else {
-	// 				startPage = currentPage - 5
-	// 				endPage = currentPage + 4
-	// 			}
-	// 		}
-	// 		const startIndex = (currentPage - 1) * pageSize
-	// 		const endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1)
-	// 		const pages = [...Array(endPage + 1 - startPage).keys()].map(
-	// 			i => startPage + i
-	// 		)
-	// 		return {
-	// 			totalItems: totalItems,
-	// 			currentPage: currentPage,
-	// 			pageSize: pageSize,
-	// 			totalPages: totalPages,
-	// 			startPage: startPage,
-	// 			endPage: endPage,
-	// 			startIndex: startIndex,
-	// 			endIndex: endIndex,
-	// 			pages: pages,
-	// 		}
-	// 	},
-	// 	[totalItems, currentPage, pageSize]
-	// )
-
 	const getPager = (totalItems, currentPage, pageSize) => {
 		currentPage = currentPage || 1
 		pageSize = pageSize || 3
@@ -92,18 +51,6 @@ const PaginationCommon = props => {
 		}
 	}
 
-	// const setPage = useCallback(
-	// 	page => {
-	// 		const { items, pageSize, onChangePage } = props
-	// 		if (page < 1 || page > pager.totalPages) {
-	// 			return
-	// 		}
-	// 		setPager(getPager(items.length, page, pageSize))
-	// 		const pageOfItems = items.slice(pager.startIndex, pager.endIndex + 1)
-	// 		onChangePage(pageOfItems)
-	// 	},
-	// 	[page]
-	// )
 	const setPage = page => {
 		const { items, pageSize, onChangePage } = props
 		if (page < 1 || page > pager.totalPages) {
