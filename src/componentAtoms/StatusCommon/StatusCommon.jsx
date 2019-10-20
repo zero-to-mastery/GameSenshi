ac166870566e5a2c6a375aa0e76ae6c75145b5a5
tylim <limteikyee@gmail.com>
2019-10-20 13:50:44 +0800

    add animation

--- src/componentAtoms/StatusCommon/StatusCommon.jsx
+++ src/componentAtoms/StatusCommon/StatusCommon.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import classNames from 'classnames'
 import styles from './styles.module.css'
+import animate from 'assets/css/animate.module.css'
 
 const StatusCommon = props => {
 	const { on, bodyOn, bodyOff } = props
 	return (
 		<div className='d-flex align-items-center'>
 			<span
-				className={classNames(styles.dot, 'mr-1', {
-					[styles.on]: on,
-					[styles.off]: !on,
-				})}
+				className={classNames(
+					styles.dot,
+					'mr-1',
+					animate.animated,
+					animate.infinite,
+					{
+						[animate.heartBeat]: on,
+						[styles.on]: on,
+						[styles.off]: !on,
+					}
+				)}
 			/>
 			<p
 				className={classNames('mb-0', {
