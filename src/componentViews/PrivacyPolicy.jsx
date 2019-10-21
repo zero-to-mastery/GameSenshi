715746f701f31432901667e5eb69471c8dbde58b
kam773 <kamilsmas@gmail.com>
2019-10-21 18:09:08 +0200

    add privacy policy tab

--- src/componentViews/PrivacyPolicy.jsx
+++ src/componentViews/PrivacyPolicy.jsx
@@ -48,7 +48,10 @@ const PrivacyPolicy = props => {
 										active: tab === 1,
 									})}
 									onClick={e => toggleTab(e, 1)}>
-									<i className='tim-icons icon-lock-circle' />
+									<i
+										style={{ fontSize: '40px' }}
+										className='tim-icons icon-lock-circle'
+									/>
 									Privacy Policy
 								</NavLink>
 							</NavItem>
@@ -58,7 +61,10 @@ const PrivacyPolicy = props => {
 										active: tab === 2,
 									})}
 									onClick={e => toggleTab(e, 2)}>
-									<i className='tim-icons icon-single-copy-04' />
+									<i
+										style={{ fontSize: '40px' }}
+										className='tim-icons icon-single-copy-04'
+									/>
 									Cookie Policy
 								</NavLink>
 							</NavItem>
@@ -67,12 +73,155 @@ const PrivacyPolicy = props => {
 					<Col lg='9' md='8'>
 						<TabContent activeTab={'viTabs' + tab}>
 							<TabPane tabId='viTabs1'>
-								Collaboratively administrate empowered markets via plug-and-play
-								networks. Dynamically procrastinate B2C users after installed
-								base benefits. <br />
-								<br />
-								Dramatically visualize customer directed convergence without
-								revolutionary ROI.
+								<h1 className='paragraph-heading'>
+									Privacy Notice for Game Senshi
+								</h1>
+								<p className='paragraph-content'>
+									This privacy policy describes how Game Senshi collects,
+									protects and uses the personally identifiable information you
+									may provide on the gamesenshi.com website and any of its
+									products or services . It also describes the choices available
+									to you regarding our use of your Personal Information and how
+									you can access and update this information. This Policy does
+									not apply to the practices of companies that we do not own or
+									control, or to individuals that we do not employ or manage.
+								</p>
+								<h4 className='paragraph-heading'>
+									Automatic collection of information
+								</h4>
+								<p className='paragraph-content'>
+									When you visit the Website our servers automatically record
+									information that your browser sends. This data may include
+									information such as your device's IP address, browser type and
+									version, operating system type and version, language
+									preferences or the webpage you were visiting before you came
+									to our Website, pages of our Website that you visit, the time
+									spent on those pages, information you search for on our
+									Website, access times and dates, and other statistics.
+								</p>
+								<h4 className='paragraph-heading'>
+									Collection of personal information
+								</h4>
+								<p className='paragraph-content'>
+									You can visit the Website without telling us who you are or
+									revealing any information by which someone could identify you
+									as a specific, identifiable individual. If, however, you wish
+									to use some of the Website's features, you will be asked to
+									provide certain Personal Information (for example, your name
+									and e-mail address). We receive and store any information you
+									knowingly provide to us when you create an account, publish
+									content, or fill any online forms on the Website. When
+									required, this information may include your email address,
+									name, credit card information, bank information, or other
+									Personal Information. You can choose not to provide us with
+									your Personal Information, but then you may not be able to
+									take advantage of some of the Website's features. Users who
+									are uncertain about what information is mandatory are welcome
+									to contact us.
+								</p>
+								<h4 className='paragraph heading'>
+									Managing personal information
+								</h4>
+								<p className='paragraph-content'>
+									You are able to access, add to, update and delete certain
+									Personal Information about you. The information you can view,
+									update, and delete may change as the Website or Services
+									change. When you update information, however, we may maintain
+									a copy of the unrevised information in our records. Some
+									information may remain in our private records after your
+									deletion of such information from your account. We will retain
+									and use your Personal Information for the period necessary to
+									comply with our legal obligations, resolve disputes, and
+									enforce our agreements unless a longer retention period is
+									required or permitted by law. We may use any aggregated data
+									derived from or incorporating your Personal Information after
+									you update or delete it, but not in a manner that would
+									identify you personally. Once the retention period expires,
+									Personal Information shall be deleted. Therefore, the right to
+									access, the right to erasure, the right to rectification and
+									the right to data portability cannot be enforced after the
+									expiration of the retention period.
+								</p>
+								<h4 className='paragraph-heading'>
+									Use and processing of collected information
+								</h4>
+								<p className='paragraph-content'>
+									Any of the information we collect from you may be used to
+									personalize your experience; improve our Website; improve
+									customer service and respond to queries and emails of our
+									customers; send newsletters; send notification emails such as
+									password reminders, updates, etc; run and operate our Website
+									and Services. Information collected automatically is used only
+									to identify potential cases of abuse and establish statistical
+									information regarding Website usage. This statistical
+									information is not otherwise aggregated in such a way that
+									would identify any particular user of the system.
+									<br />
+									We may process Personal Information related to you if one of
+									the following applies: (i) You have given your consent for one
+									or more specific purposes. Note that under some legislations
+									we may be allowed to process information until you object to
+									such processing (by opting out), without having to rely on
+									consent or any other of the following legal bases below. This,
+									however, does not apply, whenever the processing of Personal
+									Information is subject to European data protection law; (ii)
+									Provision of information is necessary for the performance of
+									an agreement with you and/or for any pre-contractual
+									obligations thereof; (iii) Processing is necessary for
+									compliance with a legal obligation to which you are subject;
+									(iv) Processing is related to a task that is carried out in
+									the public interest or in the exercise of official authority
+									vested in us; (v) Processing is necessary for the purposes of
+									the legitimate interests pursued by us or by a third party. In
+									any case, we will be happy to clarify the specific legal basis
+									that applies to the processing, and in particular whether the
+									provision of Personal Information is a statutory or
+									contractual requirement, or a requirement necessary to enter
+									into a contract.
+								</p>
+								<h4 className='paragraph-heading'>
+									Information transfer and storage
+								</h4>
+								<p className='paragraph-content'>
+									Depending on your location, data transfers may involve
+									transferring and storing your information in a country other
+									than your own. You are entitled to learn about the legal basis
+									of information transfers to a country outside the European
+									Union or to any international organization governed by public
+									international law or set up by two or more countries, such as
+									the UN, and about the security measures taken by us to
+									safeguard your information. If any such transfer takes place,
+									you can find out more by checking the relevant sections of
+									this document or inquire with us using the information
+									provided in the contact section.
+								</p>
+								<h4 className='paragraph-heading'>The rights of users</h4>
+								<p className='paragraph-content'>
+									You may exercise certain rights regarding your information
+									processed by us. In particular, you have the right to do the
+									following: (i) you have the right to withdraw consent where
+									you have previously given your consent to the processing of
+									your information; (ii) you have the right to object to the
+									processing of your information if the processing is carried
+									out on a legal basis other than consent; (iii) you have the
+									right to learn if information is being processed by us, obtain
+									disclosure regarding certain aspects of the processing and
+									obtain a copy of the information undergoing processing; (iv)
+									you have the right to verify the accuracy of your information
+									and ask for it to be updated or corrected; (v) you have the
+									right, under certain circumstances, to restrict the processing
+									of your information, in which case, we will not process your
+									information for any purpose other than storing it; (vi) you
+									have the right, under certain circumstances, to obtain the
+									erasure of your Personal Information from us; (vii) you have
+									the right to receive your information in a structured,
+									commonly used and machine readable format and, if technically
+									feasible, to have it transmitted to another controller without
+									any hindrance. This provision is applicable provided that your
+									information is processed by automated means and that the
+									processing is based on your consent, on a contract which you
+									are part of or on pre-contractual obligations thereof.
+								</p>
 							</TabPane>
 							<TabPane tabId='viTabs2'>
 								<h1>Cookie Policy for Game Senshi</h1>
