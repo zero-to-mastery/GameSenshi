a58b6bd5c211a0d07e698fda22b65357cf028594
kam773 <kamilsmas@gmail.com>
2019-11-07 20:18:38 +0100

    fix policy page bug

--- src/componentViews/PoliciesPage/PoliciesPage.jsx
+++ src/componentViews/PoliciesPage/PoliciesPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Fragment } from 'react'
 import classnames from 'classnames'
 import { stopUndefined } from 'utils'
 import { policyPageTabList } from './index'
@@ -7,14 +7,14 @@ import { Terms } from './Terms'
 import { Privacy } from './Privacy'
 import 'react-perfect-scrollbar/dist/css/styles.css'
 import PerfectScrollbar from 'react-perfect-scrollbar'
-import { ROUTE_PAGE_POLICY_COOKIES, ROUTE_PAGE_POLICY_TERMS } from 'routes'
+import { ROUTE_PAGE_POLICY_COOKIES, ROUTE_PAGE_POLICY_TERMS, ROUTE_PAGE_POLICY_PRIVACY_POLICY } from 'routes'
 import {
 	TabContent,
-	TabPane,
 	Container,
 	Row,
 	Col,
 	Nav,
+	TabPane,
 	NavItem,
 	NavLink,
 } from 'reactstrap'
@@ -22,6 +22,7 @@ import { Exports } from 'componentpMultiOrganisms'
 
 const { Footer, Link } = stopUndefined(Exports)
 
+const TO = 'to'
 const NAME = 'name'
 const ICON = 'icon'
 const POLICY = 'policy'
@@ -29,8 +30,29 @@ const PRIVACY = 'Privacy Policy'
 const COOKIE = 'Cookie Policy'
 const TERM = 'Terms And Conditions'
 
+const policies = [
+	{
+		[NAME]: PRIVACY,
+		[ICON]: 'tim-icons icon-lock-circle',
+		[POLICY]: Privacy,
+		[TO]: ROUTE_PAGE_POLICY_PRIVACY_POLICY,
+	},
+	{
+		[NAME]: COOKIE,
+		[ICON]: 'fas fa-cookie-bite',
+		[POLICY]: Cookie,
+		[TO]: ROUTE_PAGE_POLICY_COOKIES,
+	},
+	{
+		[NAME]: TERM,
+		[ICON]: 'tim-icons icon-single-copy-04',
+		[POLICY]: Terms,
+		[TO]: ROUTE_PAGE_POLICY_TERMS,
+	},
+]
+
 const PoliciesPage = props => {
-	const [TabPane, setTabPane] = useState(() => Privacy)
+	const [TabName, setTabName] = useState(() => Privacy)
 
 	useEffect(() => {
 		document.body.classList.add('index-page')
@@ -47,13 +69,13 @@ const PoliciesPage = props => {
 	useEffect(() => {
 		switch (pathname) {
 			case ROUTE_PAGE_POLICY_COOKIES:
-				setTabPane(() => Cookie)
+				setTabName(() => Cookie)
 				break
 			case ROUTE_PAGE_POLICY_TERMS:
-				setTabPane(() => Terms)
+				setTabName(() => Terms)
 				break
 			default:
-				setTabPane(() => Privacy)
+				setTabName(() => Privacy)
 		}
 	}, [pathname])
 	console.log("pathname", props)
@@ -63,61 +85,34 @@ const PoliciesPage = props => {
 				<Row style={{ height: 120 }}></Row>
 				<Row>
 					<Col lg='2' md='3'>
-						{/* <Nav
+						<Nav
 							className='nav-pills-primary nav-pills-icons flex-column'
 							pills
 							role='tablist'>
 							<NavItem>
 								{policies.map(policy => {
-									const { [NAME]: name, [ICON]: icon } = policy
+									const { [NAME]: name, [ICON]: icon, [TO]: to } = policy
 									return (
 										<NavLink
-											name={name}
 											className={classnames({
-												active: tab === name,
-											})}>
-											<i style={{ fontSize: '40px' }} className={icon} />
-											{name}
+												active: pathname.toLowerCase() === to.toLowerCase(),
+											})}
+											to={to}
+											tag={Link}>
+											<i className={`tim-icons ${icon}`} /> {name}
 										</NavLink>
 									)
 								})}
 							</NavItem>
-						</Nav> */}
-						<Nav className='flex-column nav-tabs-info' role='tablist'>
-							{/* {policyPageTabList.map((navItem, i) => {
-								const { navLink, icon, to } = navItem
-								return (
-									<Fragment key={to}>
-										<NavItem>
-											<NavLink
-												className={classnames({
-													active: pathname.toLowerCase() === to.toLowerCase(),
-												})}
-												to={to}
-												tag={Link}>
-												<i className={`tim-icons ${icon}`} /> {navLink}
-											</NavLink>
-										</NavItem>
-										{i + 1 !== policyPageTabList.length && (
-											<hr className='line-info' />
-										)}
-									</Fragment>
-								)
-							})} */}
 						</Nav>
 					</Col>
 					<Col lg='9' md='8'>
-						<TabContent activeTab={TabPane} className='mb-5'>
-							{/* {policyPageTabList.map(policy => {
-								const { [NAME]: name, [POLICY]: Policy } = policy
-								return (
-									<TabPane tabId={name}>
-										<PerfectScrollbar className='pr-3' style={{ height: 768 }}>
-											<Policy />
-										</PerfectScrollbar>
-									</TabPane>
-								)
-							})} */}
+						<TabContent activeTab={TabName} className='mb-5'>
+							<TabPane tabId={TabName}>
+								<PerfectScrollbar className='pr-3' style={{ height: 768 }}>
+									<TabName />
+								</PerfectScrollbar>
+							</TabPane>
 						</TabContent>
 					</Col>
 				</Row>
