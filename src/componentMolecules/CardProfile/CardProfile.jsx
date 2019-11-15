4fbf69b38213e925dc702606af94a592c2872b4c
kam773 <kamilsmas@gmail.com>
2019-11-15 18:23:00 +0100

    add CardProfile to Searched component

--- src/componentMolecules/CardProfile/CardProfile.jsx
+++ src/componentMolecules/CardProfile/CardProfile.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 // reactstrap components
 import {
 	Card,
@@ -8,98 +8,63 @@ import {
 	DropdownItem,
 	UncontrolledDropdown,
 	Table,
-	Col,
 } from 'reactstrap'
 
-const CARD_INDEX_NAME = 'name'
-const CARD_INDEX_IMAGE = 'image'
-const CARD_INDEX_SKILLS = 'skills'
-const CARD_INDEX_HOBBIES = 'hobbies'
-const CARD_INDEX_LEVEL = 'level'
-
 const CardProfile = props => {
-	const { cards } = props
+	const { name, src, skills, hobbies, level } = props
 	return (
-		<Fragment>
-			{cards.map(card => {
-				const {
-					[CARD_INDEX_NAME]: name,
-					[CARD_INDEX_IMAGE]: src,
-					[CARD_INDEX_SKILLS]: skills,
-					[CARD_INDEX_HOBBIES]: hobbies,
-					[CARD_INDEX_LEVEL]: level,
-				} = card
-				return (
-					<Col key={name}>
-						<Card className='card-profile'>
-							<div className='card-image'>
-								<h4 className='title'>{name}</h4>
-								<UncontrolledDropdown>
-									<DropdownToggle
-										aria-expanded={false}
-										caret
-										className='btn-icon'
-										color='link'
-										data-toggle='dropdown'
-										type='button'
-									>
-										<i className='tim-icons icon-settings-gear-63' />
-									</DropdownToggle>
-									<DropdownMenu right x-placement='bottom-end'>
-										<DropdownItem
-											href='#pablo'
-											onClick={e => e.preventDefault()}
-										>
-											See full profile
-										</DropdownItem>
-										<DropdownItem
-											href='#pablo'
-											onClick={e => e.preventDefault()}
-										>
-											Send message
-										</DropdownItem>
-									</DropdownMenu>
-								</UncontrolledDropdown>
-								<img alt='...' className='img img-raised rounded' src={src} />
-							</div>
-							<CardBody>
-								<hr className='line-primary' />
-								<Table className='tablesorter' responsive>
-									<tbody>
-										<tr>
-											<td className='text-left'>
-												<i className='tim-icons icon-atom' /> Skills
-											</td>
-											<td className='text-right'>{skills}</td>
-										</tr>
-										<tr>
-											<td className='text-left'>
-												<i className='tim-icons icon-user-run' /> Hobbies
-											</td>
-											<td className='text-right'>{hobbies}</td>
-										</tr>
-										<tr>
-											<td className='text-left'>
-												<i className='tim-icons icon-chart-bar-32' /> Level
-											</td>
-											<td className='text-right'>{level}</td>
-										</tr>
-									</tbody>
-								</Table>
-							</CardBody>
-						</Card>
-					</Col>
-				)
-			})}
-		</Fragment>
+		<Card className='card-profile'>
+			<div className='card-image'>
+				<h4 className='title'>{name}</h4>
+				<UncontrolledDropdown>
+					<DropdownToggle
+						aria-expanded={false}
+						caret
+						className='btn-icon'
+						color='link'
+						data-toggle='dropdown'
+						type='button'
+					>
+						<i className='tim-icons icon-settings-gear-63' />
+					</DropdownToggle>
+					<DropdownMenu right x-placement='bottom-end'>
+						<DropdownItem href='#pablo' onClick={e => e.preventDefault()}>
+							See full profile
+						</DropdownItem>
+						<DropdownItem href='#pablo' onClick={e => e.preventDefault()}>
+							Send message
+						</DropdownItem>
+					</DropdownMenu>
+				</UncontrolledDropdown>
+				<img alt='...' className='img img-raised rounded' src={src} />
+			</div>
+			<CardBody>
+				<hr className='line-primary' />
+				<Table className='tablesorter' responsive>
+					<tbody>
+						<tr>
+							<td className='text-left'>
+								<i className='tim-icons icon-atom' /> Skills
+							</td>
+							<td className='text-right'>{skills}</td>
+						</tr>
+						<tr>
+							<td className='text-left'>
+								<i className='tim-icons icon-user-run' /> Hobbies
+							</td>
+							<td className='text-right'>{hobbies}</td>
+						</tr>
+						<tr>
+							<td className='text-left'>
+								<i className='tim-icons icon-chart-bar-32' /> Level
+							</td>
+							<td className='text-right'>{level}</td>
+						</tr>
+					</tbody>
+				</Table>
+			</CardBody>
+		</Card>
 	)
 }
 
-export {
-	CardProfile,
-	CARD_INDEX_NAME,
-	CARD_INDEX_IMAGE,
-	CARD_INDEX_HOBBIES,
-	CARD_INDEX_SKILLS,
-	CARD_INDEX_LEVEL,
-}
+export { CardProfile }
--- src/componentMolecules/CardProfile/index.js
+++ src/componentMolecules/CardProfile/index.js
@@ -1,15 +1,12 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { CardProfile } from './CardProfile'
-import { cardPlayers, getCards } from './utils'
 
 const CardIndexPropedDefault = props => {
-	return <CardProfile cards={cardPlayers} {...props} />
+	return <CardProfile {...props} />
 }
 
 const CardSearchedPropedDefault = props => {
-	const { cards } = props
-	const cards_ = useMemo(() => getCards(cards, cardPlayers), [cards])
-	return <CardProfile cards={cards_} {...props} />
+	return <CardProfile {...props} />
 }
 
 export { CardIndexPropedDefault, CardSearchedPropedDefault }
