d3747279c432b00e8f3faef1acfcbf8407f61174
kam773 <kamilsmas@gmail.com>
2019-11-15 18:46:37 +0100

    add CardProfile to IndexPage

--- src/componentViews/IndexPage.jsx
+++ src/componentViews/IndexPage.jsx
@@ -10,6 +10,70 @@ const {
 	CarouselCommonPropedIndex,
 } = stopUndefined(Exports)
 
+const CARD_INDEX_NAME = 'name'
+const CARD_INDEX_IMAGE = 'image'
+const CARD_INDEX_SKILLS = 'skills'
+const CARD_INDEX_HOBBIES = 'hobbies'
+const CARD_INDEX_LEVEL = 'level'
+
+const cardPlayers = [
+	{
+		[CARD_INDEX_NAME]: 'Dylan Wyatt',
+		[CARD_INDEX_IMAGE]: require('assets/img/christian.jpg'),
+		[CARD_INDEX_HOBBIES]: 'Skiing, Chess',
+		[CARD_INDEX_SKILLS]: 'Dota2, LOL',
+		[CARD_INDEX_LEVEL]: 'Rising star',
+	},
+	{
+		[CARD_INDEX_NAME]: 'Mila Skylar',
+		[CARD_INDEX_IMAGE]: require('assets/img/johana.jpg'),
+		[CARD_INDEX_HOBBIES]: 'Yoga, tennis',
+		[CARD_INDEX_SKILLS]: 'Dota2, LOL',
+		[CARD_INDEX_LEVEL]: 'Rising star',
+	},
+	{
+		[CARD_INDEX_NAME]: 'Mark Johnsson',
+		[CARD_INDEX_IMAGE]: require('assets/img/mike.jpeg'),
+		[CARD_INDEX_HOBBIES]: 'Football, gym',
+		[CARD_INDEX_SKILLS]: 'Dota2, LOL',
+		[CARD_INDEX_LEVEL]: 'Rising star',
+	},
+	{
+		[CARD_INDEX_NAME]: 'Olivia Smith',
+		[CARD_INDEX_IMAGE]: require('assets/img/olivia.jpg'),
+		[CARD_INDEX_HOBBIES]: 'Football, gym',
+		[CARD_INDEX_SKILLS]: 'Dota2, LOL',
+		[CARD_INDEX_LEVEL]: 'Rising star',
+	},
+]
+
+const Cards = () => {
+	return (
+		<>
+			{cardPlayers.map(card => {
+				const {
+					[CARD_INDEX_NAME]: name,
+					[CARD_INDEX_IMAGE]: src,
+					[CARD_INDEX_LEVEL]: level,
+					[CARD_INDEX_HOBBIES]: hobbies,
+					[CARD_INDEX_SKILLS]: skills,
+				} = card
+				return (
+					<Col xs={{ size: 10, offset: 1 }} md={{ size: 3 }}>
+						<CardIndexPropedDefault
+							name={name}
+							src={src}
+							level={level}
+							hobbies={hobbies}
+							skills={skills}
+						/>
+					</Col>
+				)
+			})}
+		</>
+	)
+}
+
 const IndexPage = () => {
 	const wrapper = useRef(null)
 
@@ -47,7 +111,7 @@ const IndexPage = () => {
 							xs={{ size: 1, offset: 2 }}
 							sm={{ size: 2, offset: 2 }}
 							md={{ size: 6, offset: 2 }}
-							lg={{ size: 8, offset: 2 }}>
+						>
 							<ButtonsIconImagePropedIndex />
 						</Col>
 					</Row>
@@ -57,7 +121,7 @@ const IndexPage = () => {
 						</Col>
 					</Row>
 					<Row className='pb-5'>
-						<CardIndexPropedDefault />
+						<Cards />
 					</Row>
 					<Row>
 						<Col className='py-5'>
@@ -65,7 +129,7 @@ const IndexPage = () => {
 						</Col>
 					</Row>
 					<Row className='pb-5'>
-						<CardIndexPropedDefault />
+						<Cards />
 					</Row>
 					<Row>
 						{' '}
@@ -74,7 +138,7 @@ const IndexPage = () => {
 						</Col>
 					</Row>
 					<Row className='pb-5'>
-						<CardIndexPropedDefault />
+						<Cards />
 					</Row>
 				</Container>
 			</div>
