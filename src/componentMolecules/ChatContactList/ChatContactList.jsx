acdb59a08859b58d3afc75a53a1e9c1be909644d
kam773 <kamilsmas@gmail.com>
2019-11-17 17:49:52 +0100

    add contacts search input

--- src/componentMolecules/ChatContactList/ChatContactList.jsx
+++ src/componentMolecules/ChatContactList/ChatContactList.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+	Row,
+	Col,
+	Card,
+	CardHeader,
+	InputGroup,
+	InputGroupAddon,
+	InputGroupText,
+	Input,
+} from 'reactstrap'
+import classnames from 'classnames'
 
 const ChatContactList = props => {
+	const [searchContact, setSearchContact] = useState(null)
 	return (
-		<div>
-			<h1>contact list</h1>
-		</div>
+		<Row>
+			<Col>
+				<Card className='card-plain'>
+					<CardHeader className='mb-3'>
+						<InputGroup
+							className={classnames('form-control-lg', {
+								'input-group-focus': searchContact,
+							})}
+						>
+							<Input
+								placeholder='Search contact'
+								type='text'
+								onFocus={() => setSearchContact(true)}
+								onBlur={() => setSearchContact(false)}
+							/>
+							<InputGroupAddon addonType='append'>
+								<InputGroupText>
+									<i className='tim-icons icon-zoom-split' />
+								</InputGroupText>
+							</InputGroupAddon>
+						</InputGroup>
+					</CardHeader>
+				</Card>
+			</Col>
+		</Row>
 	)
 }
 
