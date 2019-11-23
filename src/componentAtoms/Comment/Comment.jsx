6d10b56b2f3b46b7013f040d4213d32306adf661
kam773 <kamilsmas@gmail.com>
2019-11-23 19:56:10 +0100

    refactor comment subcomponents composition

--- src/componentAtoms/Comment/Comment.jsx
+++ src/componentAtoms/Comment/Comment.jsx
@@ -26,126 +26,119 @@ const Comments = props => {
 		} = comment
 		return (
 			<Fragment key={id}>
-				<Media>
-					<CommentAvatar src={src} />
-					<CommentBody
-						id={id}
-						name={name}
-						time={time}
-						body={body}
-						commentsNumber={commentsNumber}
-					/>
-				</Media>
+				<Comment
+					name={name}
+					time={time}
+					src={src}
+					body={body}
+					commentsNumber={commentsNumber}
+				/>
 			</Fragment>
 		)
 	})
 }
 
-const CommentHeader = props => {
-	const { name, time } = props
+const Comment = props => {
+	const { body, name, time, commentsNumber, src } = props
 	return (
-		<Media heading tag='h5'>
-			{name}
-			<small className='text-muted'>· {time}</small>
-		</Media>
-	)
-}
-
-const CommentAvatar = props => {
-	const { src } = props
-	return (
-		<Fragment>
+		<Media>
 			<a className='pull-left' href='#pablo' onClick={e => e.preventDefault()}>
 				<div className='avatar'>
 					<Media alt='Avatar Comment' className='img-raised' src={src} />
 				</div>
 			</a>
-		</Fragment>
-	)
-}
-
-const CommentBody = props => {
-	const { body, name, time, commentsNumber } = props
-	return (
-		<Media body>
-			<CommentHeader name={name} time={time} />
-			<p>{body}</p>
-			<CommentFooter commentsNumber={commentsNumber} />
+			<Media body>
+				<Media heading tag='h5'>
+					{name}
+					<small className='text-muted'>· {time}</small>
+				</Media>
+				<p>{body}</p>
+				<div className='media-footer'>
+					<Button
+						className='btn-simple pull-right'
+						color='primary'
+						href='#pablo'
+						id='tooltip871944617'
+						onClick={e => e.preventDefault()}
+						size='sm'
+					>
+						<i className='tim-icons icon-send' /> Reply
+					</Button>
+					<Button
+						className='btn-simple pull-right'
+						color='danger'
+						href='#pablo'
+						onClick={e => e.preventDefault()}
+						size='sm'
+					>
+						<i className='tim-icons icon-heart-2' />
+						{commentsNumber}
+					</Button>
+				</div>
+			</Media>
 		</Media>
 	)
 }
 
-const CommentFooter = props => {
-	const { commentsNumber } = props
+const AddCommentInput = props => {
 	return (
-		<div className='media-footer'>
-			<Button
-				className='btn-simple pull-right'
-				color='primary'
-				href='#pablo'
-				id='tooltip871944617'
-				onClick={e => e.preventDefault()}
-				size='sm'
-			>
-				<i className='tim-icons icon-send' /> Reply
-			</Button>
-			<Button
-				className='btn-simple pull-right'
-				color='danger'
+		<Media className='media-post'>
+			<a
+				className='pull-left author'
 				href='#pablo'
 				onClick={e => e.preventDefault()}
-				size='sm'
 			>
-				<i className='tim-icons icon-heart-2' />
-				{commentsNumber}
-			</Button>
-		</div>
-	)
-}
-const Comment = props => {
-	const { comments } = props
-	return (
-		<div className='container'>
-			<h3 className='title text-center'>Post your comment</h3>
-			<Media className='media-post'>
-				<a
-					className='pull-left author'
-					href='#pablo'
-					onClick={e => e.preventDefault()}
-				>
-					<div className='avatar'>
-						<Media
-							alt='...'
-							className='img-raised'
-							src={require('assets/img/olivia.jpg')}
-						/>
-					</div>
-				</a>
-				<Media body>
-					<Input
-						placeholder='Write a nice reply or go home...'
-						rows='4'
-						type='textarea'
+				<div className='avatar'>
+					<Media
+						alt='...'
+						className='img-raised'
+						src={require('assets/img/olivia.jpg')}
 					/>
-					<div className='media-footer'>
-						<Button
-							className='pull-right'
-							color='primary'
-							href='#pablo'
-							onClick={e => e.preventDefault()}
-						>
-							Reply
-						</Button>
-					</div>
-				</Media>
-			</Media>
-			<div className='container'>
-				<div className='comment-wrapper'>
-					<Comments comments={comments} />
 				</div>
-			</div>
-		</div>
+			</a>
+			<Media body>
+				<Input
+					placeholder='Write a nice reply or go home...'
+					rows='4'
+					type='textarea'
+				/>
+				<div className='media-footer'>
+					<Button
+						className='pull-right'
+						color='primary'
+						href='#pablo'
+						onClick={e => e.preventDefault()}
+					>
+						Reply
+					</Button>
+				</div>
+			</Media>
+		</Media>
 	)
 }
 
-export { Comment, NAME, POSTED_TIME, AVATAR, BODY, NUM_COMMENTS, ID }
+const CommentsList = props => {
+	const { comments } = props
+	return (
+		<>
+			<div className='container'>
+				<h3 className='title text-center'>Post your comment</h3>
+				<AddCommentInput />
+			</div>
+			<div className='container'>
+				<Comments comments={comments} />
+			</div>
+		</>
+	)
+}
+
+export {
+	Comment,
+	NAME,
+	POSTED_TIME,
+	AVATAR,
+	BODY,
+	NUM_COMMENTS,
+	ID,
+	CommentsList,
+}
