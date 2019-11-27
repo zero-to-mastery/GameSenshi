6f5c96d288b27d478c247c3c379d32519030aa7d
kam773 <kamilsmas@gmail.com>
2019-11-27 17:18:44 +0100

    refactor pagination common component to have neighbour pages between active page

--- src/componentAtoms/PaginationCommon/PaginationCommon.jsx
+++ src/componentAtoms/PaginationCommon/PaginationCommon.jsx
@@ -18,32 +18,87 @@ const range = (from, to, step = 1) => {
 }
 
 const PaginationCommon = props => {
-	const { handleClick, currentPage } = props
-	const [pageCount, setPageCount] = useState(0)
+	const {
+		totalRecords,
+		pageLimit,
+		pageNeighbours,
+		onPageChanged,
+		currentPage,
+	} = props
+	const [totalPages, setTotalPages] = useState(0)
 
 	useEffect(() => {
-		const { limit, comments } = props
-		setPageCount(Math.ceil(comments.length / limit))
+		setTotalPages(Math.ceil(totalRecords / pageLimit))
 	}, [currentPage])
 
+	const fetchPageNumbers = () => {
+		const totalNumbers = pageNeighbours * 2 + 3
+		const totalBlocks = totalNumbers + 2
+
+		if (totalPages > totalBlocks) {
+			const startPage = Math.max(2, currentPage - pageNeighbours)
+			const endPage = Math.min(totalPages - 1, currentPage + pageNeighbours)
+
+			let pages = range(startPage, endPage)
+
+			const hasLeftSpill = startPage > 2
+			const hasRightSpill = totalPages - endPage > 1
+			const spillOffset = totalNumbers - (pages.length + 1)
+
+			switch (true) {
+				// handle: (1) < {5 6} [7] {8 9} (10)
+				case hasLeftSpill && !hasRightSpill: {
+					const extraPages = range(startPage - spillOffset, startPage - 1)
+					pages = [LEFT_PAGE, ...extraPages, ...pages]
+					break
+				}
+				// handle: (1) < {4 5} [6] {7 8} > (10)
+				case hasLeftSpill && hasRightSpill:
+				default: {
+					pages = [LEFT_PAGE, ...pages, RIGHT_PAGE]
+					break
+				}
+			}
+			return [1, ...pages, totalPages]
+		}
+		return range(1, totalPages)
+	}
+
+	const pages = fetchPageNumbers()
+
 	return (
-		<Pagination aria-label='Page navigation example'>
-			<PaginationItem disabled={currentPage <= 0}>
-				<PaginationLink
-					onClick={e => handleClick(e, currentPage - 1)}
-					previous
-				/>
-			</PaginationItem>
-			{[...Array(pageCount)].map((page, i) => (
-				<PaginationItem active={i === currentPage} key={i}>
-					<PaginationLink onClick={e => handleClick(e, i)}>
-						{i + 1}
-					</PaginationLink>
-				</PaginationItem>
-			))}
-			<PaginationItem disabled={currentPage >= pageCount - 1}>
-				<PaginationLink onClick={e => handleClick(e, currentPage + 1)} next />
-			</PaginationItem>
+		<Pagination aria-label='Page navigation'>
+			{pages.map((page, index) => {
+				if (page === LEFT_PAGE)
+					return (
+						<PaginationItem disabled={currentPage <= 0}>
+							<PaginationLink
+								onClick={e => onPageChanged(e, currentPage - 1)}
+								aria-label='Previous'
+								previous
+							/>
+						</PaginationItem>
+					)
+
+				if (page === RIGHT_PAGE)
+					return (
+						<PaginationItem disabled={currentPage >= totalPages - 1}>
+							<PaginationLink
+								onClick={e => onPageChanged(e, currentPage + 1)}
+								aria-label='Next'
+								next
+							/>
+						</PaginationItem>
+					)
+
+				return (
+					<PaginationItem active={currentPage === index} key={index}>
+						<PaginationLink onClick={e => onPageChanged(e, page)}>
+							{page}
+						</PaginationLink>
+					</PaginationItem>
+				)
+			})}
 		</Pagination>
 	)
 }
