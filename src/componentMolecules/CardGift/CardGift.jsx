0c23724cbf23fc08091f2a1c510f18c4df39ae0a
kam773 <kamilsmas@gmail.com>
2019-11-29 16:49:56 +0100

    replace svg icons for png

--- src/componentMolecules/CardGift/CardGift.jsx
+++ src/componentMolecules/CardGift/CardGift.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Card, CardBody } from 'reactstrap'
-
+import Image from 'material-ui-image'
+const IMAGE = 'image'
+require('assets/icons/gifts/castle.svg')
 const CardGift = props => {
 	return (
 		<Card>
 			<CardBody>
 				<h1>hello</h1>
+				<Image src={'assets/icons/gifts/castle.svg'} />
 			</CardBody>
 		</Card>
 	)
