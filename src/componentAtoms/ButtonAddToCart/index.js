76767cabc4d5538d9062231713e456d7c1d744d7
kam773 <kamilsmas@gmail.com>
2019-11-29 12:56:36 +0100

    add cart icon to buttonAddToCart

--- src/componentAtoms/ButtonAddToCart/index.js
+++ src/componentAtoms/ButtonAddToCart/index.js
@@ -4,7 +4,7 @@ import { ButtonAddToCart } from './ButtonAddToCart'
 const ButtonAddToCartPropedProfile = props => {
 	return (
 		<ButtonAddToCart size='lg' color='warning' type='button' {...props}>
-			Add to cart
+			Add to cart <i className='tim-icons icon-cart'></i>
 		</ButtonAddToCart>
 	)
 }
--- src/componentOrganisms/TabsProfile/TabsProfile.jsx
+++ src/componentOrganisms/TabsProfile/TabsProfile.jsx
@@ -21,12 +21,13 @@ const {
 	QuantityProfile,
 	IconsImagePropedTabs,
 	ButtonAddToCartPropedProfile,
+	ButtonSoundStoreSoundPropedCardUser,
 } = stopUndefined(Exports)
 
 const sections = ['Dota 2', 'PUBG', 'LOL', 'Apex Legends', 'Fortnite']
 
 const TabsProfile = props => {
-	const [tab, setTab] = useState(1)
+	const [tab, setTab] = useState('Dota 2')
 
 	const toggleTabs = (e, index) => {
 		e.preventDefault()
@@ -39,72 +40,67 @@ const TabsProfile = props => {
 					<Card>
 						<CardHeader>
 							<Nav className='nav-pills-primary' pills role='tablist'>
-								<NavItem>
-									<NavLink
-										className={classnames({
-											active: tab === 1,
-										})}
-										onClick={e => toggleTabs(e, 1)}
-										href='#pablo'
-									>
-										League of Legends
-									</NavLink>
-								</NavItem>
-								<NavItem>
-									<NavLink
-										className={classnames({
-											active: tab === 2,
-										})}
-										onClick={e => toggleTabs(e, 2)}
-										href='#pablo'
-									>
-										Settings
-									</NavLink>
-								</NavItem>
-								<NavItem>
-									<NavLink
-										className={classnames({
-											active: tab === 3,
-										})}
-										onClick={e => toggleTabs(e, 3)}
-										href='#pablo'
-									>
-										Options
-									</NavLink>
-								</NavItem>
+								{sections.map(section => {
+									return (
+										<NavItem>
+											<NavLink
+												className={classnames({
+													active: tab === section,
+												})}
+												onClick={e => toggleTabs(e, section)}
+												href='#pablo'
+											>
+												{section}
+											</NavLink>
+										</NavItem>
+									)
+								})}
 							</Nav>
 						</CardHeader>
 						<CardBody>
-							<TabContent className='tab-space' activeTab={'hTabs' + tab}>
-								<TabPane tabId='hTabs1'>
+							<TabContent className='tab-space' activeTab={tab}>
+								<TabPane tabId='Dota 2'>
 									<Table>
-										<tbody>
-											<tr>
-												<td>
-													<IconsImagePropedTabs icons='LOL' />
-												</td>
-												<td>
-													<QuantityProfile />
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<h4>Description</h4>
-													<p>
-														A veteran in MOBA and FPS games, I can give you a
-														significant boost in ranking mode.
-													</p>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<ButtonAddToCartPropedProfile />
-												</td>
-											</tr>
-										</tbody>
+										<Row className='py-2 d-flex align-items-center'>
+											<Col>
+												<IconsImagePropedTabs icons='LOL' />
+											</Col>
+											<Col>
+												<div style={{ width: '200px' }}>
+													<h4>30$ per hour</h4>
+													<h4>
+														<i className='tim-icons icon-check-2 text-success'></i>{' '}
+														40 deals completed
+													</h4>
+													<ButtonSoundStoreSoundPropedCardUser gender={true} />
+												</div>
+											</Col>
+											<Col>
+												<QuantityProfile />
+											</Col>
+										</Row>
+										<Row className='py-5 text-right'>
+											<Col md={{ offset: 8, size: 4 }}>
+												<ButtonAddToCartPropedProfile />
+											</Col>
+										</Row>
+										<Row>
+											<Col md='12'>
+												<hr className='line-primary' />
+											</Col>
+										</Row>
+										<Row>
+											<Col>
+												<h4>Description</h4>
+												<p>
+													A veteran in MOBA and FPS games, I can give you a
+													significant boost in ranking mode.
+												</p>
+											</Col>
+										</Row>
 									</Table>
 								</TabPane>
-								<TabPane tabId='hTabs2'>
+								<TabPane tabId='PUBG'>
 									Efficiently unleash cross-media information without
 									cross-media value. Quickly maximize timely deliverables for
 									real-time schemas. <br />
@@ -112,7 +108,7 @@ const TabsProfile = props => {
 									Dramatically maintain clicks-and-mortar solutions without
 									functional solutions.
 								</TabPane>
-								<TabPane tabId='hTabs3'>
+								<TabPane tabId='LOL'>
 									Completely synergize resource taxing relationships via premier
 									niche markets. Professionally cultivate one-to-one customer
 									service with robust ideas. <br />
