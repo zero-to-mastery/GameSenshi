0df1d408251bf8acd813e045719b24a53dda21ee
kam773 <kamilsmas@gmail.com>
2019-12-09 18:32:22 +0100

    add tooltip for Card grid image

--- src/componentMolecules/CardGift/CardGift.jsx
+++ src/componentMolecules/CardGift/CardGift.jsx
@@ -1,12 +1,15 @@
 import React, { Fragment } from 'react'
-import { Card, CardBody, Row, Col, Button } from 'reactstrap'
+import { Col } from 'reactstrap'
 import Image from 'material-ui-image'
 import styles from './styles.module.css'
 
 import { stopUndefined } from 'utils'
-import { Exports } from 'componentpMultiOrganisms'
+import { Exports } from 'componentaProton'
 
-const IMAGE = 'image'
+const { UncontrolledTooltip } = stopUndefined(Exports)
+
+const CARD_GIFT_IMAGE = 'image'
+const CARD_GIFT_TOOLTIP = 'tooltip'
 
 const CardGift = props => {
 	const { icons } = props
@@ -15,13 +18,24 @@ const CardGift = props => {
 			<div className={styles.bgGradient} style={{ zIndex: 99 }}>
 				<div className={styles.gridContainer}>
 					{icons.map((icon, index) => {
-						const { [IMAGE]: src } = icon
+						const {
+							[CARD_GIFT_IMAGE]: src,
+							[CARD_GIFT_TOOLTIP]: tooltip,
+						} = icon
+						const id_ = `Id${index + 1}`
 						return (
 							<Fragment key={index}>
-								<div className={`grid-item grid-item-${index + 1}`}>
+								<div id={id_} className={`grid-item-${index + 1}`}>
 									<div className={styles.imgWrapper}>
 										<Image src={src} alt={src} />
 									</div>
+									<UncontrolledTooltip
+										placement='bottom'
+										delay={0}
+										target={id_}
+									>
+										{tooltip}
+									</UncontrolledTooltip>
 								</div>
 							</Fragment>
 						)
@@ -32,4 +46,4 @@ const CardGift = props => {
 	)
 }
 
-export { CardGift, IMAGE }
+export { CardGift, CARD_GIFT_IMAGE, CARD_GIFT_TOOLTIP }
