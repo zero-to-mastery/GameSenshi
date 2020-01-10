b4a57e01faa42e37788f4b9ba7f8fcd0f30ba5a3
tylim <limteikyee@gmail.com>
2020-01-10 20:51:23 +0800

    add loader

--- src/componentViews/ProfilePage/ProfilePage.jsx
+++ src/componentViews/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { stopUndefined } from 'utils'
+import Loader from 'react-loader-spinner'
 import { Container, Row, Col } from 'reactstrap'
 import { Exports } from 'componentpMultiOrganisms'
-
+import { ROUTE_PARAM_ID } from 'routes'
 const {
 	CarouselLightBoxPropedProfile,
 	CardGiftWithProps,
@@ -34,62 +35,94 @@ const tabs = games.map((tab, index) => {
 	}
 })
 
-const ProfilePage = () => {
+const ProfilePage = props => {
+	const {
+		match: {
+			params: { [ROUTE_PARAM_ID]: id },
+		},
+	} = props
+	const [loading, setLoading] = useState(true)
+
 	return (
 		<WrapperStoreWrapperPropedProfile>
-			<Section>
-				<img alt='...' className='dots' src={require('assets/img/dots.png')} />
-				<img alt='...' className='path' src={require('assets/img/path4.png')} />
-				<Container>
-					<Row>
-						<Col>
-							<CardUserHorizontal
-								username='Mike Scheinder'
-								avatar={require('assets/img/mike.jpg')}
-								channels={channels}
-								signature='Nice to meet you!'
-								badges={badges}
-								games={games}
-								online
-								uid={'123456'}
-								favorite
-							/>
-						</Col>
-					</Row>
-					<Row className='pt-5'>
-						<Col>
-							<Tab tabs={tabs} />
-						</Col>
-					</Row>
-					<Row>
-						<Col>
-							<TextIconPropedProfileGift />
-						</Col>
-					</Row>
-					<Row>
-						<Col md='6'>
-							<CardGiftWithProps />
-						</Col>
-						<Col md='6'>
-							<CommentWithPaginationPropedDefault />
-						</Col>
-					</Row>
-				</Container>
-			</Section>
-			<Section>
-				<Container>
-					<Row>
-						<Col>
-							<TextIconPropedProfileGallery />
-						</Col>
-					</Row>
-					<Row>
-						<Col md='12'>
-							<CarouselLightBoxPropedProfile />
-						</Col>
-					</Row>
-				</Container>
-			</Section>
+			{loading ? (
+				<Row className='w-100 align-items-center' style={{ height: '100vh' }}>
+					<Col align='center'>
+						<Loader
+							type='Ball-Triangle'
+							color='#FFD700'
+							height='20vh'
+							width='20vh'
+						/>
+						<br />
+						<p style={{ 'font-size': '180%' }}>Loading, Please Wait ...</p>
+					</Col>
+				</Row>
+			) : (
+				<>
+					<Section>
+						<img
+							alt='...'
+							className='dots'
+							src={require('assets/img/dots.png')}
+						/>
+						<img
+							alt='...'
+							className='path'
+							src={require('assets/img/path4.png')}
+						/>
+						<Container>
+							<Row>
+								<Col>
+									<CardUserHorizontal
+										username='Mike Scheinder'
+										avatar={require('assets/img/mike.jpg')}
+										channels={channels}
+										signature='Nice to meet you!'
+										badges={badges}
+										games={games}
+										online
+										uid={'123456'}
+										favorite
+									/>
+								</Col>
+							</Row>
+							<Row className='pt-5'>
+								<Col>
+									<Tab tabs={tabs} />
+								</Col>
+							</Row>
+							<Row>
+								<Col>
+									<TextIconPropedProfileGift />
+								</Col>
+							</Row>
+							<Row>
+								<Col md='6'>
+									<CardGiftWithProps />
+								</Col>
+								<Col md='6'>
+									<CommentWithPaginationPropedDefault />
+								</Col>
+							</Row>
+						</Container>
+					</Section>
+					<Section>
+						<Container>
+							<Row>
+								<Col>
+									<TextIconPropedProfileGallery />
+								</Col>
+							</Row>
+							<Row>
+								<Col md='12'>
+									<CarouselLightBoxPropedProfile />
+								</Col>
+							</Row>
+						</Container>
+					</Section>
+				</>
+			)}
 		</WrapperStoreWrapperPropedProfile>
 	)
 }
