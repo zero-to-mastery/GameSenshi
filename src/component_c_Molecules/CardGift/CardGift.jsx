728ef9cf498efe0e9b64c15c2b479791483edf63
tylim <limteikyee@gmail.com>
2020-01-31 02:56:10 +0800

    gift card restyle final phase

--- src/component_c_Molecules/CardGift/CardGift.jsx
+++ src/component_c_Molecules/CardGift/CardGift.jsx
@@ -1,60 +1,110 @@
 import React, { useState, useCallback } from 'react'
 import { Input, Col, Row, Container } from 'reactstrap'
 import Image from 'material-ui-image'
-import { ModalBodyWrapper } from './styled'
 import { stopUndefined } from 'utils'
 import { Exports } from 'component_b_Atoms'
 
-const { UncontrolledTooltip, Modal } = stopUndefined(Exports)
+const { UncontrolledTooltip, Modal, Button } = stopUndefined(Exports)
 
 const CARD_GIFT_IMAGE = 'image'
-const CARD_GIFT_TOOLTIP = 'tooltip'
+const CARD_GIFT_NAME = 'tooltip'
+const CARD_GIFT_PRICE = 'price'
+const CARD_GIFT_MODAL = 'cardGiftModal'
+
+const Tooltip = props => {
+	const { id, name, price } = props
+	return (
+		<UncontrolledTooltip delay={0} target={id} innerClassName='text-left'>
+			{name}
+			<br />
+			{price}
+		</UncontrolledTooltip>
+	)
+}
 
 const CardGift = props => {
 	const [isOpen, setIsOpen] = useState(false)
-	const [modalId, setModalId] = useState('')
+	const [iconSrc, setIconSrc] = useState('')
+	const [number, setNumber] = useState('1')
+	const [name, setName] = useState('')
+	const [price, setPrice] = useState('')
 	const { icons, ...otherProps } = props
 
-	const onContinue = useCallback(e => {}, [])
+	const onContinue = useCallback(() => {}, [])
 
-	const toggleModal = useCallback((e, id) => {
-		e.preventDefault()
-		setIsOpen(isOpen => !isOpen)
-		setModalId(id)
+	const toggleModal = useCallback(() => {
+		setIsOpen(false)
+	}, [])
+
+	const onChange_ = useCallback(e => {
+		setNumber(e.target.value)
 	}, [])
 
 	return (
 		<span {...otherProps}>
 			<Modal
-				title='You have selected a gift'
+				title='Show Your Player Some Love!'
 				isOpen={isOpen}
 				toggle={toggleModal}
 				onContinue={onContinue}
-				backdrop='static'
-				modalClassName='modal-black'
+				footer='Send Gifts'
 				size='sm'
-				footer='Send a gift'
 			>
-				<ModalBodyWrapper>
-					Icon with : {modalId}
-					<Input label='No of gifts' placeholder='Select number of gifts' />
-				</ModalBodyWrapper>
+				<Container className='my-3'>
+					<Row className='justify-content-center'>
+						<Col xs='2' id={CARD_GIFT_MODAL}>
+							<Image
+								className='border border-dark rounded-0'
+								src={iconSrc}
+								alt={iconSrc}
+								style={{ height: '38px', width: '38px', padding: '0' }}
+								imageStyle={{ height: '38px', width: '38px' }}
+							/>
+						</Col>
+						<Col xs='4'>
+							<Input
+								label='No of gifts'
+								value={number}
+								type='number'
+								onChange={onChange_}
+								min='1'
+								max='99'
+							/>
+						</Col>
+					</Row>
+				</Container>
+				<Tooltip id={CARD_GIFT_MODAL} name={name} price={price} />
 			</Modal>
 			<Container className='bg-purple-gradient-2 rounded-lg'>
 				<Row xs='2' sm='3' md='4' lg='5' xl='6' className='p-2'>
-					{icons.map((icon, index) => {
-						const { [CARD_GIFT_IMAGE]: src } = icon
-						const id_ = `Id${index + 1}`
+					{icons.map(icon => {
+						const {
+							[CARD_GIFT_IMAGE]: src,
+							[CARD_GIFT_NAME]: name,
+							[CARD_GIFT_PRICE]: price,
+						} = icon
+						const id = `cardGift${name}`
+						const onClick_ = useCallback(() => {
+							setIsOpen(true)
+							setIconSrc(src)
+							setNumber(1)
+							setName(name)
+							setPrice(price)
+						}, [src])
 						return (
-							<Col onClick={toggleModal} className='p-1' id={id_} key={id_}>
-								<Image
-									className='border border-dark rounded-0'
-									src={src}
-									alt={src}
-								/>
-								<UncontrolledTooltip placement='bottom' delay={0} target={id_}>
-									Gift number {index + 1}
-								</UncontrolledTooltip>
+							<Col className='p-1' id={id} key={id}>
+								<Button
+									onClick={onClick_}
+									className='p-0 w-100'
+									baseClass='w-100'
+								>
+									<Image
+										className='border border-dark rounded-0'
+										src={src}
+										alt={src}
+									/>
+								</Button>
+								<Tooltip id={id} name={name} price={price} />
 							</Col>
 						)
 					})}
@@ -64,4 +114,4 @@ const CardGift = props => {
 	)
 }
 
-export { CardGift, CARD_GIFT_IMAGE, CARD_GIFT_TOOLTIP }
+export { CardGift, CARD_GIFT_IMAGE, CARD_GIFT_NAME, CARD_GIFT_PRICE }
--- src/component_c_Molecules/CardGift/utils.js
+++ src/component_c_Molecules/CardGift/utils.js
@@ -1,87 +1,107 @@
-import { CARD_GIFT_IMAGE, CARD_GIFT_TOOLTIP } from './CardGift'
+import { CARD_GIFT_IMAGE, CARD_GIFT_NAME, CARD_GIFT_PRICE } from './CardGift'
 
 const giftIconsPath = icon => require(`assets/icons/gifts/${icon}.png`)
 
 const gifts = [
 	{
-		[CARD_GIFT_IMAGE]: giftIconsPath('affection'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
-	},
-	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('bauble'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon1',
+		[CARD_GIFT_PRICE]: '$1',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('building'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon2',
+		[CARD_GIFT_PRICE]: '$5',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('carousel'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon3',
+		[CARD_GIFT_PRICE]: '$10',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('crowns'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon4',
+		[CARD_GIFT_PRICE]: '$15',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('cupid'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon5',
+		[CARD_GIFT_PRICE]: '$20',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('donut'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon6',
+		[CARD_GIFT_PRICE]: '$50',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('flower-bouquet'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon7',
+		[CARD_GIFT_PRICE]: '$60',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('friends'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon8',
+		[CARD_GIFT_PRICE]: '$70',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('handbag'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon9',
+		[CARD_GIFT_PRICE]: '$80',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('kitty'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon10',
+		[CARD_GIFT_PRICE]: '$90',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('lips'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon11',
+		[CARD_GIFT_PRICE]: '$100',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('lollipop'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon12',
+		[CARD_GIFT_PRICE]: '$150',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('love-message'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon13',
+		[CARD_GIFT_PRICE]: '$200',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('microphone'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon14',
+		[CARD_GIFT_PRICE]: '$250',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('positive-vote'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon15',
+		[CARD_GIFT_PRICE]: '$300',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('ring'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon16',
+		[CARD_GIFT_PRICE]: '$500',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('rose'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon17',
+		[CARD_GIFT_PRICE]: '$600',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('sunflower'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon18',
+		[CARD_GIFT_PRICE]: '$700',
 	},
 	{
 		[CARD_GIFT_IMAGE]: giftIconsPath('top-hat'),
-		[CARD_GIFT_TOOLTIP]: 'Icon',
+		[CARD_GIFT_NAME]: 'Icon19',
+		[CARD_GIFT_PRICE]: '$800',
+	},
+	{
+		[CARD_GIFT_IMAGE]: giftIconsPath('affection'),
+		[CARD_GIFT_NAME]: 'Icon20',
+		[CARD_GIFT_PRICE]: '$1000',
 	},
 ]
 
