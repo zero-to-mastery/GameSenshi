d58b76d11a11e9bbea0d1f3bcc10704fb66f98e7
tylim <limteikyee@gmail.com>
2020-02-02 03:48:07 +0800

    prevent showing error page for a split second even if senshi exist

--- src/component_g_Pages/PageProfile/utils.js
+++ src/component_g_Pages/PageProfile/utils.js
@@ -28,27 +28,29 @@ const useSenshiProfileData = (uid, currentUserUid) => {
 						const data = await docSnapshot.data()
 						if (data) {
 							setData(data)
-							setLoading(false)
 							setExist(true)
+							setLoading(false)
 						} else {
 							setExist(false)
-							setLoading(false)
 							if (uid) {
 								setErrorCode(PAGE_ERROR_CODE_NOT_FOUND)
 							} else if (currentUserUid) {
 								setErrorCode(PAGE_ERROR_CODE_NOT_A_SENSHI)
 							}
+							setLoading(false)
 							return
 						}
 					} catch (err) {
 						setExist(false)
 						setErrorCode(null)
+						setLoading(false)
 						storeModalSimpleError(err, UNEXPECTED_ERROR_CODE_17)
 					}
 				},
 				err => {
 					setExist(false)
 					setErrorCode(null)
+					setLoading(false)
 					storeModalSimpleError(err, UNEXPECTED_ERROR_CODE_16)
 				}
 			)
