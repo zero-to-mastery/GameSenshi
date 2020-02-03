85961c1479449437c34cb44417ca457e4d7df0d8
tylim <limteikyee@gmail.com>
2020-02-03 22:06:30 +0800

    add brand into helmet

--- src/component_a_Protons/Helmet/Helmet.js
+++ src/component_a_Protons/Helmet/Helmet.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Helmet as HelmetRH } from 'react-helmet'
+import { Brand } from 'utils'
 
 const Helmet = props => {
 	const { title, descr } = props
 	return (
 		<HelmetRH>
-			{title && <title>{title}</title>}
-			{descr && <meta name='description' content={descr} />}
+			{title && <title>{`${title} | ${Brand}`}</title>}
+			{descr && <meta name='description' content={`${descr} | ${Brand}`} />}
 		</HelmetRH>
 	)
 }
