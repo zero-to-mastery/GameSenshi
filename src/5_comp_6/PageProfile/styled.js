0056cf68f103ad294d3438875a2b15f79f65033d
tylim <limteikyee@gmail.com>
2020-02-05 04:46:37 +0800

    remove unused var

--- src/5_comp_6/PageProfile/styled.js
+++ src/5_comp_6/PageProfile/styled.js
@@ -3,9 +3,8 @@ import styled from 'styled-components/macro'
 import React from 'react'
 import classnames from 'classnames'
 import { stopUndefined } from '1_utils'
-import { responsiveCssGenerator, pr_4 } from '0_styled'
 import { Exports } from '5_comp_5'
-const { Col, Row } = stopUndefined(Exports)
+const { Col } = stopUndefined(Exports)
 
 const ColStyledSection = props => {
 	const { className, ...otherProps } = props
