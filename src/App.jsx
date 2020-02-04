c7e0cd5153d7029200279820af8306bda187f5b3
tylim <limteikyee@gmail.com>
2020-02-04 18:42:31 +0800

    change apolloinit folder name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { tempClient, initApollo, setClient } from 'apolloInit'
+import { tempClient, initApollo, setClient } from '0_apolloInit'
 import { ApolloProvider } from 'react-apollo'
 import { Router } from 'component_g_Pages/Router'
 import {
