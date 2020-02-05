3988c00d36827cbd15cbb4515ad91022eba812d6
tylim <limteikyee@gmail.com>
2020-02-06 01:44:46 +0800

    fix wrong value

--- src/0_styled/justify.js
+++ src/0_styled/justify.js
@@ -1,14 +1,20 @@
+const NAME = 'name'
+const ABB = 'abb'
+
 export const {
 	justify_content_start,
 	justify_content_end,
 	justify_content_center,
 	justify_content_between,
 	justify_content_around,
-} = ['start', 'end', 'center', 'between', 'around'].reduce(
-	(acc, positioning) => {
-		acc['justify_content_' + positioning] =
-			'justify-content: flex-' + positioning + ';'
-		return acc
-	},
-	{}
-)
+} = [
+	{ [ABB]: 'start', [NAME]: 'flex-start' },
+	{ [ABB]: 'end', [NAME]: 'flex-end' },
+	{ [ABB]: 'center', [NAME]: 'center' },
+	{ [ABB]: 'between', [NAME]: 'space-between' },
+	{ [ABB]: 'around', [NAME]: 'space-around' },
+].reduce((acc, positioning) => {
+	acc['justify_content_' + positioning[ABB]] =
+		'justify-content:' + positioning[NAME] + ';'
+	return acc
+}, {})
