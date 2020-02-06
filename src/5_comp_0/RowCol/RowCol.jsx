8a01007bfdf9117d6ed12d325de45e0e31dc61ec
tylim <limteikyee@gmail.com>
2020-02-06 18:24:55 +0800

    use function interpolation to prevent dynamically generate component upon dynamic styling

--- src/5_comp_0/RowCol/RowCol.jsx
+++ src/5_comp_0/RowCol/RowCol.jsx
@@ -1,56 +1,50 @@
 import styled from 'styled-components'
-import React, { memo, useMemo } from 'react'
-import {
-	Row as RowRS,
-	Col as ColRs,
-	Container as ContainerRS,
-} from 'reactstrap'
+import React, { memo } from 'react'
+import { Row as RowRS, Col as ColRs, Container } from 'reactstrap'
 import { responsiveCssGenerator } from '0_styled'
 
 const [Row, Col] = [RowRS, ColRs].map((Comp, i) => {
-	return memo(props => {
-		const { xs, sx, sm, md, lg, xl, css, ...otherProps } = props
+	const CompNew = memo(props => {
+		//eslint-disable-next-line
+		const { xs, sx, sm, md, lg, xl, ...otherProps } = props // prevent xs,sx,sm,md,lg,xl from entering Comp props
+		return <Comp {...otherProps} />
+	})
 
-		const Component = useMemo(() => {
-			const obj = { xs, sx, sm, md, lg, xl }
-			let responsiveness = {}
-			for (const props in obj) {
-				const value = i === 0 ? 100 / obj[props] : (obj[props] * 100) / 12
-				const selector = i === 0 ? '> * ' : ''
+	return styled(CompNew)`
+		&&& {
+			${props => {
+				const { xs, sx, sm, md, lg, xl } = props
+				const obj = { xs, sx, sm, md, lg, xl }
+				let responsiveness = {}
+				for (const prop in obj) {
+					const value = i === 0 ? 100 / obj[prop] : (obj[prop] * 100) / 12
+					const selector = i === 0 ? '> * ' : ''
 
-				responsiveness[props] =
-					obj[props] === undefined
-						? undefined
-						: obj[props] === '0'
-						? `&& ${selector} {
+					responsiveness[prop] =
+						obj[prop] === undefined
+							? undefined
+							: obj[prop] === '0'
+							? `${selector} {
 							display:none;
 							}`
-						: obj[props] === 'auto'
-						? `&& ${selector} {
+							: obj[prop] === 'auto'
+							? `${selector} {
 							display:block;
 							-ms-flex: 0 0 auto;
 							flex: 0 0 auto;
 							max-width: 100%;
 							}`
-						: `&& ${selector} {
+							: `${selector} {
 						display:block;
 						-ms-flex: 0 0 ${value}%;
 						flex: 0 0 ${value}%;
 						max-width: ${value}%;
 						}`
-			}
-			return styled(Comp)`
-				${responsiveCssGenerator(responsiveness)}
-				${css}
-			`
-		}, [xs, sx, sm, md, lg, xl, css])
-
-		return <Component {...otherProps} />
-	})
+				}
+				return responsiveCssGenerator(responsiveness)
+			}}
+		}
+	`
 })
-
-const Container = props => {
-	return <ContainerRS {...props} />
-}
 
 export { Row, Col, Container }
