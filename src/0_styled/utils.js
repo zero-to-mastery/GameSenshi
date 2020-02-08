8e20162fb41ae3477b30a1f5d09e648dceff808b
tylim <limteikyee@gmail.com>
2020-02-08 18:59:32 +0800

    install styled-break and styled-system

--- src/0_styled/utils.js
+++ src/0_styled/utils.js
@@ -1,112 +1,5 @@
-import styled, { css } from 'styled-components'
-
-const mediaParser = (
-	direction = '',
-	sortedBreakpoints = {},
-	targetPoint = ''
-) => {
-	const targetPointValue = sortedBreakpoints[targetPoint]
-
-	const getMaxWidth = targetPointValue => {
-		const values = Object.values(sortedBreakpoints)
-		const result = values[values.indexOf(targetPointValue) + 1]
-		if (result) {
-			return result - 0.02 + 'px'
-		} else {
-			return 'none'
-		}
-	}
-
-	const minMedia = `@media (min-width: ${targetPointValue}px)`
-	const maxMedia = `@media (max-width: ${getMaxWidth(targetPointValue)})`
-	switch (direction) {
-		case '':
-			return minMedia
-		case 'm':
-			return maxMedia
-		case 'o':
-			return `${minMedia} and ${maxMedia}`
-		default: {
-			const targetPointValue2 = sortedBreakpoints[direction]
-			if (targetPointValue2) {
-				return `${minMedia} and @media (max-width: ${getMaxWidth(
-					targetPointValue2
-				)})`
-			} else {
-				return `${minMedia} and @media (max-width: none)`
-			}
-		}
-	}
-}
-
-const responsiveCssGenerator = config => {
-	const { breakpoints, sLevel } = config
-
-	const sortedBreakpoints = Object.keys(breakpoints)
-		.sort((a, b) => {
-			return breakpoints[a] - breakpoints[b]
-		})
-		.reduce((acc, key) => {
-			acc[key] = breakpoints[key]
-			return acc
-		}, {})
-
-	const cssR = (styledCss = '') => {
-		const type = typeof styledCss
-		if (type === 'string' || Array.isArray(styledCss)) {
-			return styledCss
-		} else if (type === 'object' && styledCss) {
-			let cssString = []
-			for (const prop in styledCss) {
-				const split = prop.split('_')
-				const targetPoint = split[0]
-				if (
-					styledCss[prop] !== undefined &&
-					sortedBreakpoints[targetPoint] !== undefined
-				) {
-					const direction = split[1] || ''
-					cssString = [
-						...cssString,
-						`
-						${mediaParser(direction, sortedBreakpoints, targetPoint)} {`,
-						styledCss[prop],
-						`
-		}
-			`,
-					]
-				}
-			}
-			return cssString
-		} else {
-			return ''
-		}
-	}
-
-	const isComponentHtml = component =>
-		typeof component === 'string' ? styled[component] : styled(component)
-
-	const specificityWrapper = (styledCss = '', level = sLevel) => css`
-		${'&'.repeat(level)} {
-			${cssR(styledCss)}
-		}
-	`
-
-	const styledR = comp => (styledCss = '', level = sLevel) => {
-		return isComponentHtml(comp)`
-	 ${specificityWrapper(styledCss, level)}
-	 `
-	}
-	const styledHOC = comp => (level = sLevel) => {
-		return styled(comp)`
-			${props => {
-				const { styledCss } = props
-				return specificityWrapper(styledCss, level)
-			}}
-		`
-	}
-
-	return { cssR, styledR, styledHOC }
-}
+import { css } from 'styled-components'
+import styledBreak from 'styled-break'
 
 const config = {
 	breakpoints: {
@@ -120,6 +13,6 @@ const config = {
 	sLevel: 3,
 }
 
-const { cssR, styledR, styledHOC } = responsiveCssGenerator(config)
+const { cssR, styledR, styledHOC } = styledBreak(config)
 
 export { cssR, styledR, styledHOC, css }
